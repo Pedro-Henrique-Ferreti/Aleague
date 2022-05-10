9b2101e6ef136cbd24adcca6307bcd5a406fb56d
Pedro Henrique <phfs28@gmail.com>
2022-05-09 21:39:21 -0300

    Split variables file

--- vite.config.js
+++ vite.config.js
@@ -18,7 +18,7 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `@import "@/assets/scss/_variables.scss";`,
+        additionalData: `@import "@/assets/scss/variables/index.scss";`,
       },
     },
   },
