fc230b7241ce6feec93e4cb5060fdd95e60943fe
Pedro Henrique <phfs28@gmail.com>
2022-05-20 21:52:39 -0300

    Added logo to header

--- src/components/TheHomeHeader.vue
+++ src/components/TheHomeHeader.vue
@@ -1,11 +1,18 @@
 <template>
   <header class="home-header container-md">
-    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione quia asperiores cum a, accusamus, ipsam laudantium magni dolor eveniet molestias repellat esse iusto facere dolores tempore placeat excepturi possimus in odio temporibus! Cumque dolore sit illo inventore, facere repellat, dolorem, amet dicta quam commodi quis labore. Quibusdam consequuntur exercitationem placeat?</p>
+    <AppLogo />
   </header>
 </template>
 
+<script setup>
+import AppLogo from '@/components/AppLogo.vue';
+</script>
+
 <style lang="scss" scoped>
 .home-header {
-  --container-padding: 1.25rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 5rem;
 }
 </style>
