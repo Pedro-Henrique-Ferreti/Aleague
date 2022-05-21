4a95166a52f536d62bee4adb819d267a11fd672b
Pedro Henrique <phfs28@gmail.com>
2022-05-20 21:31:53 -0300

    Restructed Sass files and added globals

--- vite.config.js
+++ vite.config.js
@@ -18,7 +18,7 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `@import "@/assets/scss/variables/index.scss";`,
+        additionalData: `@import "@/assets/scss/globals.scss";`,
       },
     },
   },
