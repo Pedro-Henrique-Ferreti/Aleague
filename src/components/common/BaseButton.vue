310a5fbb8f012d2497fd77bf835366376bb21eb2
Pedro <phfs28@gmail.com>
2022-05-22 15:20:37 -0300

    Created the BaseButton component

---
+++ src/components/common/BaseButton.vue
@@ -0,0 +1,30 @@
+<template>
+  <component
+    class="button"
+    :is="componentTag"
+  >
+    Content here
+  </component>
+</template>
+
+<script setup>
+import { computed } from 'vue';
+
+const props = defineProps({
+  type: {
+    type: String,
+    default: 'button',
+  },
+});
+
+const componentTag = computed(() => {
+  switch (props.type) {
+    case 'link':
+      return 'router-link';
+    case 'externalLink':
+      return 'a';
+    default:
+      return 'button';
+  }
+});
+</script>
