7f53360d2c01dd72d9ee2301049509e7f3528292
Pedro <phfs28@gmail.com>
2022-05-22 15:21:01 -0300

    Added global components plugin

--- src/main.js
+++ src/main.js
@@ -2,10 +2,12 @@ import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import router from './router';
 import App from './App.vue';
+import globalComponents from '@/plugins/globalComponents';
 
 const app = createApp(App);
 
 app.use(router);
 app.use(createPinia());
+app.use(globalComponents);
 
 app.mount('#app');
