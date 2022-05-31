f88d4c0ef21b00512e0a3e92b1cbf20f7d9117f2
Pedro <phfs28@gmail.com>
2022-05-30 21:00:53 -0300

    Created the BaseIcon component

---
+++ src/components/common/BaseIcon.vue
@@ -0,0 +1,21 @@
+<template>
+  <component
+    class="icon"
+    :is="icon"
+  />
+</template>
+
+<script setup>
+import { computed } from 'vue';
+import Save from '@/components/icons/IconSave.vue';
+
+const icon = computed(() => {
+  return Save;
+});
+</script>
+
+<style lang="scss" scoped>
+.icon {
+  fill: currentColor;
+}
+</style>
--- src/plugins/globalComponents.js
+++ src/plugins/globalComponents.js
@@ -1,7 +1,9 @@
 import BaseButton from '@/components/common/BaseButton.vue';
+import BaseIcon from '@/components/common/BaseIcon.vue';
 
 export default {
   install(app) {
     app.component('BaseButton', BaseButton);
+    app.component('BaseIcon', BaseIcon);
   },
 };
