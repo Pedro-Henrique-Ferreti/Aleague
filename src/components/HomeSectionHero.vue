9c75925ef796feb3cd042caa5653c289034a6b2e
Pedro Henrique <phfs28@gmail.com>
2022-06-11 17:59:24 -0300

    Installed Swiper

--- src/components/HomeSectionHero.vue
+++ src/components/HomeSectionHero.vue
@@ -1,7 +1,21 @@
 <template>
-	<section class="hero">
-	</section>    
+  <section class="hero">
+    <Swiper
+      :slides-per-view="1"
+      :space-between="50"
+    >
+      <SwiperSlide>Lorem ipsum dolor sit amet.</SwiperSlide>
+      <SwiperSlide>Lorem ipsum dolor sit amet.</SwiperSlide>
+      <SwiperSlide>Lorem ipsum dolor sit amet.</SwiperSlide>
+    </Swiper>
+  </section>
 </template>
+
+<script setup>
+import { Swiper, SwiperSlide } from 'swiper/vue';
+
+import 'swiper/css';
+</script>
 
 <style lang="scss" scoped>
 .hero {
@@ -12,7 +26,7 @@
 		content: '';
 		display: block;
 		width: 62%;
-		height: 587px;
+		height: 100%;
 		background-color: $color--light-gray-3;
 		position: absolute;
 		top: 0;
