66f57ad49f9db1ea1b89fc66c9f5c1b098bcfc39
Pedro Henrique <phfs28@gmail.com>
2022-06-12 13:19:09 -0300

    Styled swiper pagination bullets

--- src/components/HomeSectionHero.vue
+++ src/components/HomeSectionHero.vue
@@ -1,11 +1,12 @@
 <template>
   <section class="hero">
-    <Swiper class="container-small"
+    <Swiper id="hero-swiper"
+			class="container"
 			:modules="modules"
-			:pagination="true"
+			:pagination="paginationOptions"
       :slides-per-view="1"
       :space-between="50"
-			:autoplay="{ delay: 7500 }"
+			:autoplay="autoplayOptions"
     >
       <SwiperSlide>Lorem ipsum dolor sit amet.</SwiperSlide>
       <SwiperSlide>Lorem ipsum dolor sit amet.</SwiperSlide>
@@ -23,7 +24,40 @@ import 'swiper/css';
 import 'swiper/css/autoplay';
 
 const modules = ref([Autoplay, Pagination]);
+const paginationOptions = ref({
+	clickable: true,
+});
+const autoplayOptions = ref({
+	delay: 7500,
+	disableOnInteraction: false,
+});
 </script>
+
+<style lang="scss">
+#hero-swiper {
+	max-width: 68rem;
+	.swiper-pagination-bullets {
+		margin-top: 1.5rem;
+		text-align: center;
+	}
+	.swiper-pagination-bullet {
+		display: inline-block;
+		width: 0.75rem;
+		height: 0.75rem;
+		border-radius: 50%;
+		background-color: $color--white;
+		border: 2px solid $color--text-darken;
+		cursor: pointer;
+		transition: background-color $transition--fast ease-in-out;
+		&:not(:last-of-type) {
+			margin-right: 0.75rem;
+		}
+		&.swiper-pagination-bullet-active {
+			background-color: $color--text-darken;
+		}
+	}
+}
+</style>
 
 <style lang="scss" scoped>
 .hero {
