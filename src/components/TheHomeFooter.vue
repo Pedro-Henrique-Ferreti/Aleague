724687c7918f228a3308924a15490dd70764b8bf
Pedro Henrique <phfs28@gmail.com>
2022-06-19 15:00:07 -0300

    Created TheHomeFooter.vue

---
+++ src/components/TheHomeFooter.vue
@@ -0,0 +1,41 @@
+<template>
+  <footer class="home-footer container">
+    <span class="home-footer__copyright">
+      © Aleague {{ new Date().getFullYear() }}.
+    </span>
+    Desenvolvido por
+    <a class="home-footer__link"
+      href="#"
+    >Pedro Ferreti</a> e
+    <a class="home-footer__link"
+      href="#"
+    >Guilherme Ferreti</a>.
+  </footer>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+.home-footer {
+  margin-top: 4rem;
+  padding-top: 1rem;
+  padding-bottom: 1rem;
+  background-color: $color--light-gray-2;
+  text-align: center;
+  @include for-large-tablet-portrait-up {
+    padding-top: 1.5rem;
+    padding-bottom: 1.5rem;
+  }
+  &__copyright {
+    @include for-large-tablet-portrait-down {
+      display: block;
+    }
+  }
+  &__link {
+    color: $color--text-darken;
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+}
+</style>
--- src/layouts/HomeLayout.vue
+++ src/layouts/HomeLayout.vue
@@ -1,8 +1,10 @@
 <template>
   <TheHomeHeader />
   <slot />
+  <TheHomeFooter />
 </template>
 
 <script setup>
 import TheHomeHeader from '@/components/TheHomeHeader.vue';
+import TheHomeFooter from '@/components/TheHomeFooter.vue';
 </script>
