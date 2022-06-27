1658d6b1aef6895981767b5657833c5a8de358bf
Pedro Henrique <phfs28@gmail.com>
2022-06-27 20:26:31 -0300

    Created the AuthForm component

---
+++ src/components/AuthForm.vue
@@ -0,0 +1,33 @@
+<template>
+  <form
+    class="auth-form"
+    @submit.prevent="$emit('submit')"
+  >
+    <div class="auth-form__fields">
+      <slot />
+    </div>
+    <div class="auth-form__footer">
+      <slot name="footer" />
+    </div>
+  </form>
+</template>
+
+<script setup>
+defineEmits(['submit']);
+</script>
+
+<style lang="scss" scoped>
+.auth-form {
+  &__fields {
+    display: flex;
+    flex-direction: column;
+    gap: 1.5rem;
+  }
+  &__footer {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    margin-top: 2rem;
+  }
+}
+</style>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,7 +1,18 @@
 <template>
   <AuthHeading>Acesse sua conta</AuthHeading>
+  <AuthForm>
+    <template v-slot:footer>
+      <BaseButton>
+        Fazer login
+      </BaseButton>
+      <BaseButton color="gray">
+        Recuperar senha
+      </BaseButton>
+    </template>
+  </AuthForm>
 </template>
 
 <script setup>
 import AuthHeading from '@/components/AuthHeading.vue';
+import AuthForm from '@/components/AuthForm.vue';
 </script>
