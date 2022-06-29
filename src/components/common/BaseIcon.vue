09470d4f1ae348c302d1d747cdce9a09394fe69a
Pedro Henrique <phfs28@gmail.com>
2022-06-29 20:38:19 -0300

    Added the check icon

--- src/components/common/BaseIcon.vue
+++ src/components/common/BaseIcon.vue
@@ -4,11 +4,12 @@
 
 <script setup>
 import { computed } from 'vue';
+import ArrowFoward from '@/components/icons/IconArrowFoward.vue';
+import Check from '@/components/icons/IconCheck.vue';
 import Close from '@/components/icons/IconClose.vue';
+import ErrorCircle from '@/components/icons/IconErrorCircle.vue';
 import Menu from '@/components/icons/IconMenu.vue';
 import Save from '@/components/icons/IconSave.vue';
-import ArrowFoward from '@/components/icons/IconArrowFoward.vue';
-import ErrorCircle from '@/components/icons/IconErrorCircle.vue';
 
 const props = defineProps({
   icon: {
@@ -18,17 +19,19 @@ const props = defineProps({
 });
 
 const icon = computed(() => {
-  switch (props.icon.toLowerCase()) {
+  switch (props.icon.toLowerCase().replaceAll('-', '')) {
+    case 'arrowfoward':
+      return ArrowFoward;
+    case 'check':
+      return Check;
     case 'close':
       return Close;
+    case 'errorcircle':
+      return ErrorCircle;
     case 'menu':
       return Menu;
     case 'save':
       return Save;
-    case 'arrowfoward':
-      return ArrowFoward;
-    case 'errorcircle':
-      return ErrorCircle;
     default:
       return '';
   }
---
+++ src/components/icons/IconCheck.vue
@@ -0,0 +1,18 @@
+<template>
+  <svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="currentColor"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <g clip-path="url(#clip0_205_2842)">
+      <path d="M7.83738 16.8082L2.35438 11.3252L0 13.6796L7.83738 21.517L24 5.35438L21.6456 3L7.83738 16.8082Z" />
+    </g>
+    <defs>
+      <clipPath id="clip0_205_2842">
+        <path d="M0 0H24V24H0V0Z" />
+      </clipPath>
+    </defs>
+  </svg>
+</template>
