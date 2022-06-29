b43b5f802866b0d977dabb62594743323e72a570
Pedro Henrique <phfs28@gmail.com>
2022-06-29 11:08:23 -0300

    Added TextField wrapper and icon styles

--- src/components/common/TextField.vue
+++ src/components/common/TextField.vue
@@ -1,17 +1,30 @@
 <template>
-  <div class="text-field">
+  <div
+    class="text-field"
+    :class="textFieldClasses"
+  >
     <label
       class="text-field__label"
       v-if="label"
       v-text="label"
       :for="id"
     />
-    <BaseInput
-      :type="type"
-      :id="id"
-      :aria-label="label ? null : ariaLabel"
-      v-model="inputValue"
-    />
+    <div class="text-field__input-wrapper">
+      <BaseInput
+        class="text-field__input"
+        :type="type"
+        :id="id"
+        :aria-label="label ? null : ariaLabel"
+        v-model="inputValue"
+      />
+      <BaseIcon
+        class="text-field__icon"
+        icon="save"
+      />
+    </div>
+    <small class="text-field__message">
+      Por favor, digite seu email
+    </small>
   </div>
 </template>
 
@@ -43,6 +56,10 @@ const props = defineProps({
   },
 });
 
+const textFieldClasses = computed(() => ({
+  'text-field--error': true,
+}));
+
 const inputValue = computed({
   get() {
     return props.modelValue;
@@ -55,10 +72,36 @@ const inputValue = computed({
 
 <style lang="scss" scoped>
 .text-field {
+  --color: #{$color--text};
+  --message-display: none;
+
+  &--error {
+    --message-display: block;
+    --color: #{$color--danger};
+  }
   &__label {
     display: block;
     margin-bottom: 0.5rem;
     font-size: 0.875rem;
+    font-weight: $font-weight--semibold;
+  }
+  &__input-wrapper {
+    display: flex;
+    align-items: center;
+    position: relative;
+  }
+  &__icon {
+    width: 1rem;
+    height: 1rem;
+    fill: var(--color);
+    position: absolute;
+    right: 0.75rem;
+  }
+  &__message {
+    display: var(--message-display);
+    margin-top: 0.5rem;
+    color: var(--color);
+    font-size: 0.8125rem;
     font-weight: $font-weight--semibold;
   }
 }
