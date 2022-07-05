322e1ce631a2e6d4b6728e5e5bf1fc6691b423d3
Pedro Henrique <phfs28@gmail.com>
2022-07-05 16:23:56 -0300

    Added register form fields

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,7 +1,57 @@
 <template>
   <AuthHeading>Crie sua conta</AuthHeading>
+  <AuthForm>
+    <TextField
+      id="register--field-name"
+      label="Nome de usuário"
+    />
+    <TextField
+      id="register--field-email"
+      type="email"
+      label="Email"
+    />
+    <TextField
+      id="register--field-password"
+      label="Senha"
+    />
+    <TextField
+      id="register--field-confirm-password"
+      label="Repita a senha"
+    />
+    <template #footer>
+      <BaseButton>Criar conta</BaseButton>
+    </template>
+  </AuthForm>
+  <span class="login">
+    Já possui uma conta?
+    <router-link
+      class="login__link"
+      :to="{ name: 'login' }"
+    >
+      Faça login
+    </router-link>
+  </span>
 </template>
 
 <script setup>
+import TextField from '@/components/common/TextField.vue';
 import AuthHeading from '@/components/AuthHeading.vue';
+import AuthForm from '@/components/AuthForm.vue';
 </script>
+
+<style lang="scss" scoped>
+.login {
+  display: block;
+  width: 100%;
+  margin-top: 5rem;
+  color: $color--text-darken;
+  text-align: center;
+  &__link {
+    color: $color--secondary;
+    @include focus-ring;
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+}
+</style>
