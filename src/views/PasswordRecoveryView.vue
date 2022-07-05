d4427e81477d728369cac497cf5487fc4016c276
Pedro Henrique <phfs28@gmail.com>
2022-07-05 16:49:04 -0300

    Added PasswordRecovery form fields

--- src/views/PasswordRecoveryView.vue
+++ src/views/PasswordRecoveryView.vue
@@ -1,7 +1,34 @@
 <template>
   <AuthHeading>Recupere sua senha</AuthHeading>
+  <p class="text-darken">
+    Para recuperar sua senha, digite o endereço de email que você utiliza para acessar sua conta.
+  </p>
+  <AuthForm class="password-recovery__form">
+    <TextField
+      id="password-recovery--field-email"
+      type="email"
+      label="Email"
+    />
+    <template #footer>
+      <BaseButton>Recuperar senha</BaseButton>
+      <BaseButton
+        color="gray"
+        :to="{ name: 'login' }"
+      >
+        Voltar
+      </BaseButton>
+    </template>
+  </AuthForm>
 </template>
 
 <script setup>
+import TextField from '@/components/common/TextField.vue';
 import AuthHeading from '@/components/AuthHeading.vue';
+import AuthForm from '@/components/AuthForm.vue';
 </script>
+
+<style lang="scss" scoped>
+.password-recovery__form {
+  margin-top: 2rem;
+}
+</style>
