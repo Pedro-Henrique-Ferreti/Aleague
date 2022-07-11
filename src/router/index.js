957ca1fc6788fc5f8685b4b747eb0cdf3ba99708
Pedro Henrique <phfs28@gmail.com>
2022-07-11 19:37:19 -0300

    Created the VerifyEmail view

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import HomeView from '@/views/HomeView.vue';
 import LoginView from '@/views/LoginView.vue';
 import RegisterView from '@/views/RegisterView.vue';
 import PasswordRecoveryView from '@/views/PasswordRecoveryView.vue';
+import VerifyEmailView from '@/views/VerifyEmailView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -29,6 +30,12 @@ const router = createRouter({
       path: '/recuperar-senha',
       name: 'password-recovery',
       component: PasswordRecoveryView,
+      meta: { layout: 'auth' },
+    },
+    {
+      path: '/verificar-email',
+      name: 'verify-email',
+      component: VerifyEmailView,
       meta: { layout: 'auth' },
     },
   ],
---
+++ src/views/VerifyEmailView.vue
@@ -0,0 +1,7 @@
+<template>
+  <AuthHeading>Verifique seu email</AuthHeading>  
+</template>
+
+<script setup>
+import AuthHeading from '@/components/AuthHeading.vue';
+</script>
