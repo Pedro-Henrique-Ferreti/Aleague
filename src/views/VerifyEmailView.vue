02fd62597b8ed54a63803ab921cb9dc409b4a8a5
Pedro Henrique <phfs28@gmail.com>
2022-07-19 17:00:48 -0300

    Created the method to resend the email verification code

--- src/views/VerifyEmailView.vue
+++ src/views/VerifyEmailView.vue
@@ -3,7 +3,10 @@
   <span class="email-address">
     {{ authStore.user.email }}
   </span>
-  <AuthForm>
+  <AuthForm
+    :error-message="errorMessage"
+    @close-alert="errorMessage = ''"
+  >
     <p
       v-if="$route.query.from === 'register'"
       class="text-darken"
@@ -16,6 +19,10 @@
     >
       Bem-vindo de volta! Antes de começar, digite o código de seis dígitos recebido no e-mail informado.
     </p>
+    <EnterCode
+      :is-loading="isSendingCode"
+      @resend-code="resendCode"
+    />
     <template #footer>
       <BaseButton>Continuar</BaseButton>
     </template>
@@ -23,11 +30,29 @@
 </template>
 
 <script setup>
+import { ref } from 'vue';
 import { useAuthStore } from '@/stores/authStore';
+
 import AuthHeading from '@/components/AuthHeading.vue';
 import AuthForm from '@/components/AuthForm.vue';
+import EnterCode from '@/components/EnterCode.vue';
 
 const authStore = useAuthStore();
+
+const isSendingCode = ref(false);
+const errorMessage = ref('');
+
+async function resendCode() {
+  isSendingCode.value = true;
+
+  try {
+    await authStore.sendEmailVerificationCode();
+  } catch (error) {
+    errorMessage.value = 'Falha ao enviar o código de verificação. Por favor, tente novamente.';
+  } finally {
+    isSendingCode.value = false;
+  }
+}
 </script>
 
 <style lang="scss" scoped>
