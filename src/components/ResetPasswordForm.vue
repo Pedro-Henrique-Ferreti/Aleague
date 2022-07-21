a3a7e3679564fc96c9844614f49281ede870db8d
Pedro Henrique <phfs28@gmail.com>
2022-07-21 16:24:52 -0300

    Added ResetPassword form validation

--- src/components/ResetPasswordForm.vue
+++ src/components/ResetPasswordForm.vue
@@ -7,18 +7,23 @@
   </p>
   <AuthForm
     class="reset-password-form"
+    @submit="submitForm"
   >
     <TextField
+      v-model.lazy="password"
       id="reset-password--field-new-password"
       type="password"
       label="Nova senha"
-      v-model.lazy="password"
+      :dirty="v$.password.$dirty"
+      :error-message="v$.password.$errors[0]?.$message"
     />
     <TextField
+      v-model.lazy="passwordConfirmation"
       id="reset-password--field-password-confirmation"
       type="password"
       label="Repita a nova senha"
-      v-model.lazy="passwordConfirmation"
+      :dirty="v$.passwordConfirmation.$dirty"
+      :error-message="v$.passwordConfirmation.$errors[0]?.$message"
     />
     <template #footer>
       <BaseButton
@@ -31,13 +36,46 @@
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
+import useVuelidate from '@vuelidate/core';
+import { helpers, sameAs, minLength } from '@vuelidate/validators';
+import { required } from '@/helpers/i18nValidators';
 import TextField from './common/TextField.vue';
 import AuthForm from './AuthForm.vue';
 import AuthHeading from './AuthHeading.vue';
 
+const minLenghtValidator = helpers.withMessage(
+  'Sua nova senha deve ter ao menos 8 caracteres.',
+  minLength(8),
+);
+
 const password = ref('');
 const passwordConfirmation = ref('');
+
+const rules = computed(() => ({
+  password: {
+    required,
+    minLength: minLenghtValidator,
+  },
+  passwordConfirmation: {
+    required,
+    minLength: minLenghtValidator,
+    sameAsRef: helpers.withMessage('As senhas informadas não são iguais.', sameAs(password)),
+  },
+}));
+
+const v$ = useVuelidate(rules, {
+  password,
+  passwordConfirmation,
+}, { $autoDirty: true });
+
+async function submitForm() {
+  const formIsValid = await v$.value.$validate();
+
+  if (!formIsValid) {
+    return;
+  }
+}
 </script>
 
 <style lang="scss" scoped>
