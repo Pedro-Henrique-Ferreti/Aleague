fa43fe1e3f87a6a6b0e29b3afe749b23ed66b2f9
Pedro Henrique <phfs28@gmail.com>
2022-07-21 17:01:10 -0300

    Created ResetPasswordSuccess.vue

---
+++ src/components/ResetPasswordSuccess.vue
@@ -0,0 +1,42 @@
+<template>
+  <AuthHeading>Redefina sua senha</AuthHeading>
+  <div class="reset-success__image-wrapper">
+    <img
+      class="reset-success__image"
+      src="/images/smartphone-verified.svg"
+      alt="Girl next to smartphone with check sign"
+    />
+  </div>
+  <p class="text-darken">
+    Sua senha foi redefinida com sucesso!
+  </p>
+  <BaseButton
+    class="reset-success__button"
+    :to="{ name: 'login' }"
+  >
+    Acessar minha conta
+  </BaseButton>
+</template>
+
+<script setup>
+import AuthHeading from './AuthHeading.vue';
+</script>
+
+<style lang="scss" scoped>
+.reset-success {
+  &__image-wrapper {
+    display: flex;
+    justify-content: center;
+    width: 100%;
+    margin-bottom: 2rem;
+  }
+  &__image {
+    width: auto;
+    height: 9.5rem;
+  }
+  &__button{
+    width: 100%;
+    margin-top: 2rem;
+  }
+}
+</style>
--- src/views/ResetPasswordView.vue
+++ src/views/ResetPasswordView.vue
@@ -1,7 +1,15 @@
 <template>
-  <ResetPasswordForm />
+  <ResetPasswordSuccess v-if="showSuccessMessage" />
+  <ResetPasswordForm
+    v-else
+    @password-reset="showSuccessMessage = true"
+  />
 </template>
 
 <script setup>
+import { ref } from 'vue';
 import ResetPasswordForm from '@/components/ResetPasswordForm.vue';
+import ResetPasswordSuccess from '@/components/ResetPasswordSuccess.vue';
+
+const showSuccessMessage = ref(false);
 </script>
