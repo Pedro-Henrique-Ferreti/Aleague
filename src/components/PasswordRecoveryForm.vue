663d4a6cfb067331bba5233073fcf7f813749b34
Pedro Henrique <phfs28@gmail.com>
2022-07-21 12:01:18 -0300

    Created the RecoveryPasswordForm and RecoveryPasswordSuccess components

---
+++ src/components/PasswordRecoveryForm.vue
@@ -0,0 +1,90 @@
+<template>
+  <AuthHeading>Recupere sua senha</AuthHeading>
+  <p class="text-darken">
+    Para recuperar sua senha, digite o endereço de email que você utiliza para acessar sua conta.
+  </p>
+  <AuthForm
+    class="password-recovery__form"
+    :error-message="errorMessage"
+    @close-alert="errorMessage = ''"
+    @submit="submitForm"
+  >
+    <TextField
+      v-model.lazy="email"
+      id="password-recovery--field-email"
+      type="email"
+      label="Email"
+      :dirty="v$.email.$dirty"
+      :error-message="v$.email.$errors[0]?.$message"
+    />
+    <template #footer>
+      <BaseButton
+        type="submit"
+        :is-loading="isLoading"
+      >
+        Recuperar senha
+      </BaseButton>
+      <BaseButton
+        color="gray"
+        :to="{ name: 'login' }"
+      >
+        Voltar
+      </BaseButton>
+    </template>
+  </AuthForm>
+</template>
+
+<script setup>
+import { ref, computed } from 'vue';
+import useVuelidate from '@vuelidate/core';
+import { required, email as emailValidator } from '@/helpers/i18nValidators';
+import { useAuthStore } from '@/stores/authStore';
+import TextField from './common/TextField.vue';
+import AuthHeading from './AuthHeading.vue';
+import AuthForm from './AuthForm.vue';
+
+const emit = defineEmits(['email-sent']);
+
+const authStore = useAuthStore();
+
+const email = ref('');
+const isLoading = ref(false);
+const errorMessage = ref('');
+
+const rules = computed(() => ({
+  email: {
+    required,
+    email: emailValidator,
+  },
+}));
+
+const v$ = useVuelidate(rules, { email }, { $autoDirty: true });
+
+async function submitForm() {
+  errorMessage.value = '';
+
+  const formIsValid = await v$.value.$validate();
+
+  if (!formIsValid) {
+    return;
+  }
+
+  isLoading.value = true;
+
+  try {
+    await authStore.sendPasswordRecoveryEmail(email.value);
+
+    emit('email-sent');
+  } catch (error) {
+    errorMessage.value = error.message;
+  } finally {
+    isLoading.value = false;
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.password-recovery__form {
+  margin-top: 2rem;
+}
+</style>
---
+++ src/components/PasswordRecoverySuccess.vue
@@ -0,0 +1,43 @@
+<template>
+  <AuthHeading>E-mail enviado</AuthHeading>
+  <div class="recovery-success__image-wrapper">
+    <img
+      class="recovery-success__image"
+      src="/images/mail-sent.svg"
+      alt="Email sent successfully"
+    />
+  </div>
+  <p class="text-darken">
+    Verifique em sua caixa de entrada por um link para recuperar sua senha.
+  </p>
+  <BaseButton
+    class="recovery-success__button"
+    color="gray"
+    :to="{ name: 'login' }"
+  >
+    Voltar ao login
+  </BaseButton>
+</template>
+
+<script setup>
+import AuthHeading from './AuthHeading.vue';
+</script>
+
+<style lang="scss" scoped>
+.recovery-success {
+  &__image-wrapper {
+    display: flex;
+    justify-content: center;
+    width: 100%;
+    margin-bottom: 2rem;
+  }
+  &__image {
+    width: auto;
+    height: 9.5rem;
+  }
+  &__button{
+    width: 100%;
+    margin-top: 2rem;
+  }
+}
+</style>
