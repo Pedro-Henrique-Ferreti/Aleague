fa7ade462bd1e9c5044da1b4756e04305843f596
Pedro Henrique <phfs28@gmail.com>
2022-07-25 11:49:07 -0300

    Created the LoadingIndicator component

---
+++ src/components/LoadingIndicator.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="loading-indicator">
+    <BaseIcon
+      class="loading-indicator__icon"
+      icon="loading"
+    />
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+.loading-indicator {
+  display: grid;
+  place-items: center;
+  &__icon {
+    width: 4.5rem;
+    height: 4.5rem;
+    stroke: $color--primary;
+  }
+}
+</style>
