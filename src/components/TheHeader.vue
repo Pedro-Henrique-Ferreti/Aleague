3d61f6b5510931ee125ba6f55fb60744c6082ca6
Pedro Henrique <phfs28@gmail.com>
2022-07-25 22:29:30 -0300

    Created TheHeader component

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,36 @@
+<template>
+  <header class="header">
+    <div class="header__container container-medium">
+      <router-link
+        class="header__logo-wrapper"
+        :to="{ name: 'home'}"
+      >
+        <AppLogo />
+      </router-link>
+    </div>
+  </header>
+</template>
+
+<script setup>
+import AppLogo from './AppLogo.vue';
+</script>
+
+<style lang="scss" scoped>
+.header {
+  height: 4.5rem;
+  background-color: $color--white;
+  border-bottom: 1px solid $color--light-gray-1;
+  &__container {
+    display: flex;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+  }
+  &__logo-wrapper {
+    @include for-large-tablet-portrait-down {
+      display: none;
+    }
+    @include focus-ring;
+  }
+}
+</style>
