d7ff7f91b45c7d8a953a6237c1924cd8158b0bb0
Pedro Henrique <phfs28@gmail.com>
2022-07-25 22:54:33 -0300

    Created the AppTitle component

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1 class="heading">
+    <slot />
+  </h1>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+.heading {
+  margin-bottom: 1rem;
+  color: $color--text-darken;
+  font-size: 1.5rem;
+  font-weight: $font-weight--semibold;
+  @include for-desktop-up {
+    font-size: 1.875rem;
+  }
+}
+</style>
