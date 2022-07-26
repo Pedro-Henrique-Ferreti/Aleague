75a15b372c4c8c1bc7f308cabf5cb05bbb9515ec
Pedro Henrique <phfs28@gmail.com>
2022-07-25 22:55:54 -0300

    Added AppTitle and dynamic greeting message

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,27 @@
 <template>
-  <h1>home view</h1>
+  <main class="container-small">
+    <AppTitle>{{ greetingMessage }}</AppTitle>
+    <p>Bem-vindo ao Aleague! Você pode escolher uma das opções abaixo.</p>
+  </main>
 </template>
+
+<script setup>
+import { computed } from 'vue';
+import { useAuthStore } from '@/stores/authStore';
+import AppTitle from '@/components/AppTitle.vue';
+
+const authStore = useAuthStore();
+
+const greetingMessage = computed(() => {
+  const curentHour = new Date().getTime();
+  let greeting = 'Boa noite';
+
+  if (curentHour < 13) {
+    greeting = 'Bom dia';
+  } else if (curentHour < 19) {
+    greeting = 'Boa tarde';
+  }
+
+  return `${greeting}, ${authStore.user.username}`;
+});
+</script>
