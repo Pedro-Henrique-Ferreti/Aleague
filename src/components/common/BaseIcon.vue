8dac67a7672938bb8831f6bc9871bc70f0d62eb6
Pedro Henrique <phfs28@gmail.com>
2022-08-09 12:07:57 -0300

    Added icons Clock, EnvelopeCheck, Logout, and Pencil

--- src/components/common/BaseIcon.vue
+++ src/components/common/BaseIcon.vue
@@ -8,13 +8,17 @@ import ArrowFoward from '@/components/icons/IconArrowFoward.vue';
 import Bell from '@/components/icons/IconBell.vue';
 import Check from '@/components/icons/IconCheck.vue';
 import Chevron from '@/components/icons/IconChevron.vue';
+import Clock from '@/components/icons/IconClock.vue';
 import Close from '@/components/icons/IconClose.vue';
+import EnvelopeCheck from '@/components/icons/IconEnvelopeCheck.vue';
 import ErrorCircle from '@/components/icons/IconErrorCircle.vue';
 import Gear from '@/components/icons/IconGear.vue';
 import Home from '@/components/icons/IconHome.vue';
 import HomeOutline from '@/components/icons/IconHomeOutline.vue';
 import Loading from '@/components/icons/IconLoading.vue';
+import Logout from '@/components/icons/IconLogout.vue';
 import Menu from '@/components/icons/IconMenu.vue';
+import Pencil from '@/components/icons/IconPencil.vue';
 import Plus from '@/components/icons/IconPlus.vue';
 import Save from '@/components/icons/IconSave.vue';
 import TeamShield from '@/components/icons/IconTeamShield.vue';
@@ -39,8 +43,12 @@ const icon = computed(() => {
       return Check;
     case 'chevron':
       return Chevron;
+    case 'clock':
+      return Clock;
     case 'close':
       return Close;
+    case 'envelopecheck':
+      return EnvelopeCheck;
     case 'errorcircle':
       return ErrorCircle;
     case 'gear':
@@ -51,8 +59,12 @@ const icon = computed(() => {
       return HomeOutline;
     case 'loading':
       return Loading;
+    case 'logout':
+      return Logout;
     case 'menu':
       return Menu;
+    case 'pencil':
+      return Pencil;
     case 'save':
       return Save;
     case 'plus':
