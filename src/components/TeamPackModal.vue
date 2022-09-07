c3b9395e914d846f2b80eacd0dbc4b1dd65f0f22
Pedro Henrique <phfs28@gmail.com>
2022-09-07 11:36:27 -0300

    Created the TeamPackModalPacksList component

--- src/components/TeamPackModal.vue
+++ src/components/TeamPackModal.vue
@@ -5,22 +5,57 @@
     :show="show"
     @close="$emit('close')"
   >
-
+    <div class="team-packs">
+      <PacksList :show-loading-indicator="isLoading" />
+    </div>
   </AppModal>
 </template>
 
 <script lang="ts" setup>
+import { ref, watch } from 'vue';
+import { useTeamsStore } from '@/stores/teamsStore';
 import AppModal from './AppModal.vue';
+import PacksList from './TeamPackModalPacksList.vue';
+import { useNotificationStore } from '@/stores/notificationStore';
 
 defineEmits(['close']);
-defineProps({
+
+const props = defineProps({
   show: {
     type: Boolean,
     required: true,
   },
 });
+
+const { getTeamPacks } = useTeamsStore();
+const { openSnackbarNotification } = useNotificationStore();
+
+watch(() => props.show, () => {
+  if (props.show) {
+    loadTeamPacks();
+  }
+});
+
+const isLoading = ref(true);
+
+async function loadTeamPacks() {
+  isLoading.value = true;
+
+  try {
+    await getTeamPacks();
+  } catch (error: any) {
+    openSnackbarNotification({
+      type: 'error',
+      message: 'Falha ao carregar os pacotes de equipe.',
+    });
+  } finally {
+    isLoading.value = false;
+  }
+}
 </script>
 
 <style lang="scss" scoped>
-
+.team-packs {
+  min-height: 20rem;
+}
 </style>
---
+++ src/components/TeamPackModalPacksList.vue
@@ -0,0 +1,39 @@
+<template>
+  <p class="packs-list__description">
+    Pacotes de equipe contém equipes famosas de várias ligas ao redor do mundo. Escolha um dos pacotes abaixo para visualizar a lista de equipes que serão criadas ao aplicar o pacote.
+  </p>
+  <Transition name="fade">
+    <LoadingIndicator v-if="showLoadingIndicator" />
+    <div
+      v-else
+      class="packs-list-grid"
+    >
+      <div
+        v-for="pack in teamsStore.teamPacks"
+        :key="pack.id"
+      >
+        {{ pack.name }}
+      </div>
+    </div>
+  </Transition>
+</template>
+
+<script lang="ts" setup>
+import { useTeamsStore } from '@/stores/teamsStore';
+import LoadingIndicator from './LoadingIndicator.vue';
+
+defineProps({
+  showLoadingIndicator: {
+    type: Boolean,
+    required: true,
+  },
+});
+
+const teamsStore = useTeamsStore();
+</script>
+
+<style lang="scss" scoped>
+.packs-list__description {
+  margin-bottom: 2rem;
+}
+</style>
