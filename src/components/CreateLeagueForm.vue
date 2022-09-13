614ad73bd7e0b5f60f576d52d91e3ce6c1b3fbb5
Pedro Henrique <phfs28@gmail.com>
2022-09-13 16:56:01 -0300

    Created the CreateLeagueForm component

---
+++ src/components/CreateLeagueForm.vue
@@ -0,0 +1,33 @@
+<template>
+  <form
+    class="create-league-form"
+    @submit.prevent="$emit('submit')"
+  >
+    <slot />
+    <div class="create-league-form__footer">
+      <slot name="footer" />
+    </div>
+  </form>
+</template>
+
+<script lang="ts" setup>
+defineEmits(['submit']);
+</script>
+
+<style lang="scss" scoped>
+.create-league-form {
+  display: flex;
+  flex-direction: column;
+  min-height: 30rem;
+  &__footer {
+    display: grid;
+    gap: 1rem;
+    margin-top: auto;
+    padding-top: 3rem;
+    @include for-tablet-portrait-up {
+      grid-template-columns: repeat(2, 11rem);
+      gap: 1.5rem;
+    }
+  }
+}
+</style>
