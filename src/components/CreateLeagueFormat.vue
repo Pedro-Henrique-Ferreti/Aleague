87df2762353fe16492b2b8090c08453419f76b1b
Pedro Henrique <phfs28@gmail.com>
2022-09-13 16:56:41 -0300

    Created the CreateLeagueLayout component

--- src/components/CreateLeagueFormat.vue
+++ src/components/CreateLeagueFormat.vue
@@ -6,9 +6,45 @@
     <p>Bem-vindo a criação de campeonatos! Para começar, escolha nome e formato para o seu campeonato.</p>
   </PageHeader>
   <CreateLeagueStepper :current-step="1" />
+  <CreateLeagueLayout>
+    <CreateLeagueForm>
+      <AppLargeField
+        placeholder="Digite o nome do campeonato"
+        icon="football"
+      />
+      <div class="league-format-form">
+        <h2 class="league-format-form__title">
+          Escolha o formato
+        </h2>
+      </div>
+      <template #footer>
+        <AppButton color="gray">
+          Voltar
+        </AppButton>
+        <AppButton>Próximo passo</AppButton>
+      </template>
+    </CreateLeagueForm>
+  </CreateLeagueLayout>
 </template>
 
 <script lang="ts" setup>
+import AppLargeField from './AppLargeField.vue';
 import PageHeader from './PageHeader.vue';
 import CreateLeagueStepper from './CreateLeagueStepper.vue';
+import CreateLeagueForm from './CreateLeagueForm.vue';
+import CreateLeagueLayout from './CreateLeagueLayout.vue';
 </script>
+
+<style lang="scss" scoped>
+.league-format-form {
+  margin-top: 2.5rem;
+  &__title {
+    margin-bottom: 1rem;
+    font-size: 1.125rem;
+    font-weight: $font-weight--semibold;
+  }
+  @include for-tablet-landscape-up {
+    margin-top: 3.5rem;
+  }
+}
+</style>
---
+++ src/components/CreateLeagueLayout.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="create-league-layout">
+    <slot />
+    <img
+      class="create-league-layout__image"
+      src="/images/marketer.svg"
+      alt=""
+      draggable="false"
+    />
+  </div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style lang="scss" scoped>
+.create-league-layout {
+  display: grid;
+  &__image {
+    display: none;
+    width: auto;
+    height: 25rem;
+    margin-left: auto;
+  }
+  @include for-tablet-landscape-up {
+    grid-template-columns: 50% auto;
+    .create-league-layout__image {
+      display: block;
+    }
+  }
+  @include for-desktop-up {
+    .create-league-layout__image {
+      height: 29.5rem;
+    }
+  }
+}
+</style>
