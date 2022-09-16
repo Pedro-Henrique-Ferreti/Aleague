a633c6cf256bbf3846c6f9ef367ef2969bdcfdc9
Pedro Henrique <phfs28@gmail.com>
2022-09-16 08:59:59 -0300

    Added the RadioControl icon

---
+++ src/components/icons/IconRadioControl.vue
@@ -0,0 +1,53 @@
+<template>
+  <svg
+    width="14"
+    height="14"
+    viewBox="0 0 14 14"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    class="radio-input"
+    :class="{ 'radio-input--checked': checked }"
+  >
+    <template v-if="checked">
+      <circle
+        cx="7"
+        cy="7"
+        r="6.5"
+        fill="white"
+        style="stroke: var(--color)"
+      />
+      <circle
+        cx="6.99998"
+        cy="7.00016"
+        r="4.66667"
+        style="fill: var(--color)"
+      />
+    </template>
+    <circle
+      v-else
+      cx="7"
+      cy="7"
+      r="6.5"
+      fill="white"
+      style="stroke: var(--color)"
+    />
+  </svg>
+</template>
+
+<script lang="ts" setup>
+defineProps({
+  checked: {
+    type: Boolean,
+    default: false,
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.radio-input {
+  --color: #{$color--light-gray-1};
+  &--checked {
+    --color: #{$color--tertiary};
+  }
+}
+</style>
