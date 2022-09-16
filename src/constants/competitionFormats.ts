c8931a48eacfa0cc94f0bd5300331266bf11d6fb
Pedro Henrique <phfs28@gmail.com>
2022-09-16 17:06:17 -0300

    Added the "isAvailable" property to competitionFormats

--- src/constants/competitionFormats.ts
+++ src/constants/competitionFormats.ts
@@ -4,17 +4,20 @@ export const COMPETITION_FORMATS = {
     value: 'league',
     image: '/images/leagues/league-format-icon.svg',
     description: 'Cada um dos competidores enfrenta todos os demais. Ao final, aquele que obtiver mais pontos é o campeão. Pode ser disputado em turno único ou em dois turnos (turno e returno).',
+    isAvailable: true,
   },
   cup: {
     name: 'Copa',
     value: 'cup',
     image: '/images/leagues/cup-format-icon.svg',
     description: 'Dividido em duas fases. Na primeira fase, as equipes são dividias em grupos, onde podem se enfrentar em um turno ou dois turnos. Um determinado número de equipes é promovido para a próxima fase, que é disputada em formato eliminatório.',
+    isAvailable: false,
   },
   playOff: {
     name: 'Eliminatórias',
     value: 'play-off',
     image: '/images/leagues/play-off-format-icon.svg',
     description: 'Cada equipe competidora disputa um certo número de partidas, até ser promovida à próxima fase. A equipe perdedora de cada disputa é eliminada da competição.',
+    isAvailable: false,
   },
 };
