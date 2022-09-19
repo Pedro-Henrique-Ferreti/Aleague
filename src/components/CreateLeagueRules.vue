ae868b230ada7de3724297045d5f1750a64c8cae
Pedro Henrique <phfs28@gmail.com>
2022-09-19 11:18:37 -0300

    Added CreateLeagueLayout and CreateLeagueForm to the rules page

--- src/components/CreateLeagueRules.vue
+++ src/components/CreateLeagueRules.vue
@@ -3,10 +3,63 @@
     <template #title>
       Criar campeonato
     </template>
-    <p>Agora você deve escolher as regras para o seu campeonato pontos corridos.</p>
+    <p>Agora você deve escolher as regras para o seu campeonato <b>pontos corridos</b>.</p>
   </PageHeader>
+  <CreateLeagueLayout>
+    <CreateLeagueForm>
+      <AppTransition name="fade">
+        <LoadingIndicator v-if="isLoadingLeague" />
+      </AppTransition>
+      <template #footer>
+        <AppButton
+          color="gray"
+          :to="{ name: 'create-league-format', params: { id: $route.params.id }}"
+        >
+          Voltar
+        </AppButton>
+        <AppButton>Próximo passo</AppButton>
+      </template>
+    </CreateLeagueForm>
+  </CreateLeagueLayout>
 </template>
 
 <script lang="ts" setup>
-import PageHeader from '@/components/PageHeader.vue';
+import { ref } from 'vue';
+import { useRoute } from 'vue-router';
+import { useLeaguesStore } from '@/stores/leaguesStore';
+import { useNotificationStore } from '@/stores/notificationStore';
+
+import AppTransition from './AppTransition.vue';
+import PageHeader from './PageHeader.vue';
+import CreateLeagueLayout from './CreateLeagueLayout.vue';
+import CreateLeagueForm from './CreateLeagueForm.vue';
+import LoadingIndicator from './LoadingIndicator.vue';
+
+const route = useRoute();
+const leaguesStore = useLeaguesStore();
+const { openSnackbarNotification } = useNotificationStore();
+
+const isLoadingLeague = ref(true);
+
+// League data
+const leagueName = ref('');
+
+getLeague();
+
+async function getLeague() {
+  isLoadingLeague.value = true;
+
+  try {
+    const { name } = await leaguesStore.getLeague(route.params.id as string);
+
+    leagueName.value = name;
+  } catch (error: any) {
+    openSnackbarNotification({
+      type: 'error',
+      message: error.message,
+    });    
+  } finally {
+    isLoadingLeague.value = false;
+  }
+}
 </script>
