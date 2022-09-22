22ac7c4476d9ac70c2f7627f7c35b1a8431ccf30
Pedro Henrique <phfs28@gmail.com>
2022-09-22 15:32:00 -0300

    Add and display participants

--- src/views/CreateLeagueParticipantsView.vue
+++ src/views/CreateLeagueParticipantsView.vue
@@ -14,6 +14,22 @@
           :league-name="league.name"
           :teams-count="league.numberOfTeams"
         />
+        <AddParticipantsField
+          v-model="teamName"
+          class="participants-search-field"
+          placeholder="Digite o nome da equipe"
+          :autocomplete-options="autocompleteOptions"
+          @add-team="addTeamToParticipantsList"
+        />
+        <div class="participants-grid">
+          <ParticipantCard
+            v-for="n in league.numberOfTeams"
+            :key="n"
+            :index="n"
+            :name="participants[n - 1] || ''"
+            @remove="removeTeamFromParticipantList(participants[n - 1] || '')"
+          />
+        </div>
       </div>
     </AppTransition>
     <template #footer>
@@ -29,19 +45,23 @@
 </template>
 
 <script lang="ts" setup>
-import { ref } from 'vue';
+import { computed, onMounted, ref } from 'vue';
 import { useRoute } from 'vue-router';
+import { useTeamsStore } from '@/stores/teamsStore';
 import { useLeaguesStore } from '@/stores/leaguesStore';
 import { useNotificationStore } from '@/stores/notificationStore';
 
 import AppTransition from '@/components/AppTransition.vue';
 import LoadingIndicator from '@/components/LoadingIndicator.vue';
 import PageHeader from '@/components/PageHeader.vue';
+import AddParticipantsField from '@/components/AddParticipantsField.vue';
 import CreateLeagueStepper from '@/components/CreateLeagueStepper.vue';
 import CreateLeagueForm from '@/components/CreateLeagueForm.vue';
 import CreateLeagueFormHeader from '@/components/CreateLeagueFormHeader.vue';
+import ParticipantCard from '@/components/CreateLeagueParticipantCard.vue';
 
 const route = useRoute();
+const teamsStore = useTeamsStore();
 const leaguesStore = useLeaguesStore();
 const { openSnackbarNotification } = useNotificationStore();
 
@@ -53,13 +73,15 @@ const league = ref({
 });
 const isLoadingLeague = ref(true);
 
-getLeague();
-
-async function getLeague() {
+// OnMounted hook
+onMounted(async () => {
   isLoadingLeague.value = true;
 
   try {
-    const { name, numberOfTeams } = await leaguesStore.getLeague(league.value.id);
+    const [{ name, numberOfTeams }] = await Promise.all([
+      leaguesStore.getLeague(league.value.id),
+      teamsStore.getTeams(),
+    ]);
 
     league.value.name = name;
     league.value.numberOfTeams = numberOfTeams || 0;
@@ -71,5 +93,56 @@ async function getLeague() {
   } finally {
     isLoadingLeague.value = false;
   }
+});
+
+// Search field
+const teamName = ref('');
+const autocompleteOptions = computed(() => teamsStore.teams.map(({ name }) => name ));
+
+// Participants
+const participants = ref<(string | null)[]>([]);
+
+function addTeamToParticipantsList() {
+  participants.value.push(teamName.value);
+
+  teamName.value = '';
+}
+
+function removeTeamFromParticipantList(teamName: string) {
+  const teamIndex = participants.value.indexOf(teamName);
+
+  if (teamIndex > -1) {
+    participants.value.splice(teamIndex, 1);
+  }
 }
 </script>
+
+<style lang="scss" scoped>
+.participants-search-field {
+  margin-top: 2rem;
+  @include for-large-tablet-landscape-up {
+    width: 50%;
+    margin: 0 auto;
+    margin-top: 3.5rem;
+  }
+}
+.participants-grid {
+  --columns: 1;
+  display: grid;
+  grid-template-columns: repeat(var(--columns), 1fr);
+  gap: 1rem;
+  margin-top: 2rem;
+  padding-top: 1.5rem;
+  border-top: 1px solid $color--light-gray-1;
+  @include for-tablet-portrait-up {
+    --columns: 2;
+    gap: 1.5rem 1rem;
+  }
+  @include for-large-tablet-landscape-up {
+    --columns: 3;
+  }
+  @include for-desktop-up {
+    --columns: 4;
+  }
+}
+</style>
