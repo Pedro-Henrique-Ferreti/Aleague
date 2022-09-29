ce033f40d204464b56ccf4acf8e626eb784bf712
Pedro Henrique <phfs28@gmail.com>
2022-09-29 10:38:17 -0300

    Created the modal component to delete a league

--- src/components/LeagueDetailsActions.vue
+++ src/components/LeagueDetailsActions.vue
@@ -3,12 +3,27 @@
     <SectionHeader heading="h3">
       Ações
     </SectionHeader>
-    <AppButton color="dangerGray">
+    <AppButton
+      color="dangerGray"
+      @click="toggleShowModalDelete"
+    >
       Excluir campeonato
     </AppButton>
+    <LeagueDetailsActionsModalDelete
+      :show="showModalDelete"
+      @close="toggleShowModalDelete"
+    />
   </section>
 </template>
 
 <script lang="ts" setup>
+import { ref } from 'vue';
 import SectionHeader from './SectionHeader.vue';
+import LeagueDetailsActionsModalDelete from './LeagueDetailsActionsModalDelete.vue';
+
+const showModalDelete = ref(false);
+
+function toggleShowModalDelete() {
+  showModalDelete.value = !showModalDelete.value;
+}
 </script>
---
+++ src/components/LeagueDetailsActionsModalDelete.vue
@@ -0,0 +1,112 @@
+<template>
+  <AppModal
+    id="league-actions-modal-delete"
+    title="Excluir campeonato"
+    size="small"
+    :show="show"
+    @close="closeModal"
+  >
+    <p class="text-center">
+      Você tem certeza que deseja excluir este campeonato? Esta ação não poderá ser desfeita. As estatísticas das suas equipes serão alteradas.
+    </p>
+    <div class="modal-delete__input-wrapper">
+      <p>
+        Por favor, digite <span class="modal-delete__league-name">{{ league?.name }}</span> para confirmar.
+      </p>
+      <BaseInput
+        v-model.trim="confirmationValue"
+        class="modal-delete__input"
+      />
+    </div>
+    <div class="modal-delete__footer">
+      <AppButton
+        color="gray"
+        @click="closeModal"
+      >
+        Cancelar
+      </AppButton>
+      <AppButton
+        :disabled="!confirmationValueIsValid"
+        :is-loading="isDeletingLeague"
+        @click="deleteLeague"
+      >
+        Confirmar e excluir
+      </AppButton>
+    </div>
+  </AppModal>
+</template>
+
+<script lang="ts" setup>
+import type { LeagueWithStandings } from '@/types/League';
+import { computed, inject, ref, type Ref } from 'vue';
+import { useRouter } from 'vue-router';
+import { useNotificationStore } from '@/stores/notificationStore';
+import BaseInput from './common/BaseInput.vue';
+import AppModal from './AppModal.vue';
+
+const router = useRouter();
+const { openSnackbarNotification } = useNotificationStore();
+
+const emit = defineEmits(['close']);
+defineProps({
+  show: {
+    type: Boolean,
+    required: true,
+  },
+});
+
+function closeModal() {
+  emit('close');
+}
+
+// Injected values
+const league = inject<Ref<LeagueWithStandings>>('league');
+
+// Delete league
+const isDeletingLeague = ref(false);
+const confirmationValue = ref('');
+
+const confirmationValueIsValid = computed(() => {
+  return confirmationValue.value.toLowerCase() === league?.value.name.toLowerCase();
+});
+
+async function deleteLeague() {
+  isDeletingLeague.value = true;
+
+  try {
+    closeModal();
+    router.push({ name: 'leagues' });
+  } catch (error: any) {
+    openSnackbarNotification({
+      type: 'error',
+      message: error.message,
+    });
+  } finally {
+    isDeletingLeague.value = false;
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.modal-delete {
+  &__input-wrapper {
+    margin-top: 2rem;
+  }
+  &__league-name {
+    color: $color--text-darken;
+    font-weight: $font-weight--semibold;
+  }
+  &__input {
+    margin-top: 0.5rem;
+  }
+  &__footer {
+    display: grid;
+    gap: 1rem;
+    margin-top: 2rem;
+    @include for-tablet-portrait-up {
+      grid-template-columns: repeat(2, 1fr);
+      gap: 1.5rem;
+    }
+  }
+}
+</style>
