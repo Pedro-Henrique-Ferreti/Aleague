2f2ea134b1cfc9709b32f0ae555d86bee8a11932
Pedro Henrique <phfs28@gmail.com>
2022-10-06 16:56:30 -0300

    Created the LeagueMatches table

--- src/components/LeagueMatches.vue
+++ src/components/LeagueMatches.vue
@@ -1,15 +1,125 @@
 <template>
-  <div class="league-matches"></div>
+  <div class="league-matches">
+    <div class="league-matches__content">
+      <AppButton
+        icon-rounded
+        class="league-matches__save-button"
+        icon-left="save"
+      >
+        Salvar
+      </AppButton>
+      <div class="league-matches__table">
+        <div class="league-matches__table-header">
+          <TableButton
+            aria-label="First gameweek"
+            icon="double-chevron"
+          />
+          <TableButton
+            aria-label="Previous gameweek"
+            rotate="left"
+          />
+          <span class="league-matches__table-gameweek">
+            Rodada {{ currentGameweek }}
+          </span>
+          <TableButton aria-label="Next gameweek" />
+          <TableButton
+            aria-label="Last gameweek"
+            icon="double-chevron"
+            rotate="right"
+          />
+        </div>
+        <div class="league-matches__table-body">
+          <LoadingIndicator
+            v-if="isLoadingGameweek"
+            class="league-matches__loading-indicator"
+          />
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts" setup>
-// import {  } from 'vue';
+import type { Gameweek } from '@/types/Game';
+import { inject, ref } from 'vue';
+import { useNotificationStore } from '@/stores/notificationStore';
+import { useLeaguesStore } from '@/stores/leaguesStore';
+import { INJECTION_KEYS } from '@/constants';
+
+import LoadingIndicator from './LoadingIndicator.vue';
+import TableButton from './LeagueMatchesTableButton.vue';
+
+const { openSnackbarNotification } = useNotificationStore();
+const leaguesStore = useLeaguesStore();
+
+// Injected values
+const league = inject(INJECTION_KEYS.league);
+
+// Gameweek
+const isLoadingGameweek = ref(false);
+const currentGameweek = ref(1);
+const gameweek = ref<Gameweek>([]);
+
+getGameweek();
+
+async function getGameweek() {
+  isLoadingGameweek.value = true;
+
+  try {
+    gameweek.value = await leaguesStore.getGameweek({
+      leagueId: league?.value.hashid || '',
+      gameweekNumber: currentGameweek.value,
+    });
+  } catch (error: any) {
+    openSnackbarNotification({
+      type: 'error',
+      message: error.message,
+    });
+  } finally {
+    isLoadingGameweek.value = false;
+  }
+}
 </script>
 
 <style lang="scss" scoped>
 .league-matches {
-  width: 20rem;
-  height: 25rem;
-  background-color: $color--tertiary;
+  &__content {
+    display: flex;
+    flex-direction: column-reverse;
+    align-items: flex-end;
+    gap: 1rem;
+    @include for-desktop-up {
+      flex-direction: column;
+    }
+  }
+  &__save-button {
+    margin-left: auto;
+  }
+  &__table {
+    --spacing: 1rem;
+    width: 20rem;
+    border-radius: 0.25rem;
+    border: 1px solid $color--light-gray-1;
+  }
+  &__table-header {
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+    padding: 0.5rem var(--spacing);
+    background-color: $color--light-gray-2;
+    border-bottom: 1px solid $color--light-gray-1;
+  }
+  &__table-gameweek {
+    flex: 1;
+    font-weight: $font-weight--semibold;
+    text-align: center;
+  }
+  &__table-body {
+    padding: var(--spacing);
+  }
+  &__loading-indicator {
+    --size: 3rem;
+    height: 10rem;
+  }
 }
 </style>
---
+++ src/components/LeagueMatchesTableButton.vue
@@ -0,0 +1,60 @@
+<template>
+  <button
+    class="league-matches-button"
+    type="button"
+  >
+    <BaseIcon
+      class="league-matches-button__icon"
+      :icon="icon"
+      :aria-label="ariaLabel"
+      :data-rotate="rotate || null"
+    />
+  </button>
+</template>
+
+<script lang="ts" setup>
+import type { PropType } from 'vue';
+
+defineProps({
+  icon: {
+    type: String,
+    default: 'chevron',
+  },
+  ariaLabel: {
+    type: String,
+    required: true,
+  },
+  rotate: {
+    type: String as PropType<'left' | 'right'>,
+    default: '',
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.league-matches-button {
+  @include focus-ring;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 2rem;
+  height: 2rem;
+  background-color: $color--white;
+  border: 1px solid $color--light-gray-1;
+  border-radius: 0.25rem;
+  transition: background-color $transition--fastest ease-in;
+  &:hover {
+    background-color: $color--light-gray-2;
+  }
+  &__icon {
+    height: 0.85rem;
+    fill: $color--primary;
+    &[data-rotate="left"] {
+      transform: rotate(180deg);
+    }
+    &[data-rotate="right"] {
+      transform: rotate(-180deg);
+    }
+  }
+}
+</style>
