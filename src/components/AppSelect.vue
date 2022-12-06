4e405ba95e8afe0a6d5bce960cf5ce0cc4a90f41
Pedro Henrique <phfs28@gmail.com>
2022-12-06 15:41:27 -0300

    Created the AppSelect component

---
+++ src/components/AppSelect.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="app-select">
+    <label
+      class="app-select__label"
+      v-if="label"
+      v-text="label"
+      :for="id"
+    />
+    <select
+      v-model="inputValue"
+      class="app-select__input"
+      :id="id"
+      :readonly="readonly"
+      :aria-label="label ? undefined : ariaLabel"
+    >
+      <slot />
+    </select>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { computed } from 'vue';
+
+const emit = defineEmits(['update:modelValue']);
+
+const props = defineProps({
+  modelValue: {
+    type: [String, Number],
+    default: '',
+  },
+  id: {
+    type: String,
+    required: true,
+  },
+  label: {
+    type: String,
+    default: '',
+  },
+  ariaLabel: {
+    type: String,
+    default: '',
+  },
+  readonly: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const inputValue = computed({
+  get() {
+    return props.modelValue;
+  },
+  set(value) {
+    emit('update:modelValue', value);
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.app-select {
+  &__label {
+    display: block;
+    margin-bottom: 0.5rem;
+    font-size: 0.875rem;
+    font-weight: $font-weight--semibold;
+  }
+  &__input {
+    width: 100%;
+    min-height: 2.75rem;
+    padding: 0 1rem;
+    background-color: $color--white;
+    color: $color--text-darken;
+    border: 1px solid $color--gray-1;
+    border-radius: 0.5rem;
+    outline: none;
+    cursor: pointer;
+    &:focus {
+      border-color: $color--text-lighten;
+    }
+  }
+}
+</style>
