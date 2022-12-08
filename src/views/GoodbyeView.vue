d6699613ef9f2b94ddd86f83e3898c5da805425a
Pedro Henrique <phfs28@gmail.com>
2022-12-08 10:10:22 -0300

    Added content to the GoodbyeView

--- src/views/GoodbyeView.vue
+++ src/views/GoodbyeView.vue
@@ -1,8 +1,51 @@
 <template>
-  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Exercitationem eius sunt asperiores soluta culpa itaque harum sequi non nobis delectus. Animi provident sit, numquam quae esse sunt molestiae accusantium placeat.  
+  <header class="container-extra-small">
+    <AppLogo class="goodbye__logo" />
+  </header>
+  <main class="goodbye container-extra-small">
+    <h1 class="goodbye__title text-darken">
+      Obrigado por utilizar o Aleague!
+    </h1>
+    <img
+      class="goodbye__image"
+      src="/images/teamwork.svg"
+      alt="Teamwork"
+    />
+    <p>
+      Sua conta foi excluída com sucesso. Esperamos ver você novamente!
+    </p>
+    <AppButton
+      class="goodbye__button"
+      :to="{ name: 'landing' }"
+    >
+      Ir a página principal
+    </AppButton>
+  </main>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import AppButton from '@/components/AppButton.vue';
+import AppLogo from '@/components/AppLogo.vue';
+</script>
 
 <style lang="scss" scoped>
+.goodbye {
+  text-align: center;
+  &__logo {
+    margin: 3rem 0;
+  }
+  &__title {
+    font-size: 1.875rem;
+    font-weight: $font-weight--semibold;
+  }
+  &__image {
+    max-height: 21.875rem;
+    margin: 2rem auto 2.5rem auto;
+  }
+  &__button {
+    width: 100%;
+    max-width: 18rem;
+    margin-top: 3rem;
+  }
+}
 </style>
