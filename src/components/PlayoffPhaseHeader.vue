8b0d9020ca269f84d68cbb39b548582926181b52
Pedro Henrique <phfs28@gmail.com>
2022-12-23 21:16:21 -0300

    Created the PlayoffPhaseHeader component

---
+++ src/components/PlayoffPhaseHeader.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="phase-header | playoff-phase-grid">
+    <span>Oitavas de final</span>
+    <span>Quartas de final</span>
+    <span>Semi-final</span>
+  </div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style lang="scss" scoped>
+.phase-header {
+  place-items: center;
+  height: 2.5rem;
+  margin-bottom: 2rem;
+  background-color: $color--light-gray-2;
+  border-radius: 0.5rem;
+  font-size: 1.25rem;
+  font-weight: $font-weight--semibold;
+}
+</style>
--- src/components/PlayoffStandings.vue
+++ src/components/PlayoffStandings.vue
@@ -1,15 +1,31 @@
 <template>
   <div class="playoff-standings">
-    <PhaseToggle />
+    <div class="playoff-standings__controls">
+      <PhaseToggle />
+    </div>
+    <PhaseHeader />
+    <div class="playoff-phase-grid">
+      <p>Oitavas de final</p>
+      <p>Quartas de final</p>
+      <p>Semi-final</p>
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
 import PhaseToggle from './CompetitionPhaseToggle.vue';
+import PhaseHeader from './PlayoffPhaseHeader.vue';
 </script>
 
 <style lang="scss" scoped>
 .playoff-standings {
   margin-top: 1.5rem;
+  &__controls {
+    display: flex;
+    align-items: flex-end;
+    width: 100%;
+    min-height: 2.75rem;
+    margin-bottom: 1.5rem;
+  }
 }
 </style>
