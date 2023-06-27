271ff8624da00e3fe4f711f27de9f644869b3158
Pedro Henrique <phfs28@gmail.com>
2023-06-27 11:06:07 -0300

    Updated Team Slot styles

--- src/components/TeamSlot.vue
+++ src/components/TeamSlot.vue
@@ -27,23 +27,24 @@ defineProps({
 <style lang="scss" scoped>
 .team-slot {
   --card-height: 3.5rem;
-  --font-size: 1.125rem;
   --icon-wrapper-size: 2rem;
+  --color: #{$color--tertiary-400};
   &[data-small="true"] {
     --card-height: 1.5rem;
-    --font-size: 1rem;
     --icon-wrapper-size: auto;
-    color: $color--text-lighten;
+    --color: #{$color--text-lighten};
+  }
+  &[data-small="false"] {
+    justify-content: center;
+    background-image: url("data:image/svg+xml,%3csvg width='100%25' height='100%25' xmlns='http://www.w3.org/2000/svg'%3e%3crect width='100%25' height='100%25' fill='none' rx='8' ry='8' stroke='%23B0CDFAFF' stroke-width='2' stroke-dasharray='8%2c8' stroke-dashoffset='0' stroke-linecap='square'/%3e%3c/svg%3e");
+    font-weight: $font-weight--semibold;
   }
 
   display: flex;
   align-items: center;
   gap: 1rem;
   height: var(--card-height);
-  &[data-small="false"] {
-    justify-content: center;
-    background-image: url("data:image/svg+xml,%3csvg width='100%25' height='100%25' xmlns='http://www.w3.org/2000/svg'%3e%3crect width='100%25' height='100%25' fill='none' rx='8' ry='8' stroke='%23B0CDFAFF' stroke-width='2' stroke-dasharray='8%2c8' stroke-dashoffset='0' stroke-linecap='square'/%3e%3c/svg%3e");
-  }
+  color: var(--color);
   &__icon-wrapper {
     display: flex;
     justify-content: center;
@@ -57,9 +58,6 @@ defineProps({
     width: 1rem;
     height: 1rem;
     fill: $color--tertiary-400;
-  }
-  &__text {
-    font-size: var(--font-size);
   }
 }
 </style>
