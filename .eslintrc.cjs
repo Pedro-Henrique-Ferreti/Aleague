032e6245bee23552d33bda19978c16050adcaa0b
Pedro Henrique <phfs28@gmail.com>
2024-06-13 16:20:24 -0300

    Added eslint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,39 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
+require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
   root: true,
-  'extends': [
-    'plugin:vue/vue3-essential',
+  extends: [
+    'plugin:vue/vue3-strongly-recommended',
     'eslint:recommended',
-    '@vue/eslint-config-typescript'
+    '@vue/eslint-config-airbnb-with-typescript',
   ],
+  rules: {
+    // eslint rules
+    'quotes': [2, 'single'],
+    'semi': 2,
+    'no-trailing-spaces': 'error',
+    'comma-dangle': ['error', 'always-multiline'],
+    'camelcase': 2,
+    'linebreak-style': 'off',
+    'import/first': 'off',
+    'import/prefer-default-export': 'off',
+    'consistent-return': 'off',
+    'no-restricted-globals': 'off',
+    '@typescript-eslint/no-unused-vars': ['error'],
+
+    // vue.js rules
+    'vue/html-self-closing': ['error', {
+      'html': {
+        'void': 'always',
+        'normal': 'always',
+        'component': 'always',
+      },
+    }],
+    'vuejs-accessibility/form-control-has-label': 'off',
+    'vue/no-setup-props-destructure': 'off',
+  },
   parserOptions: {
-    ecmaVersion: 'latest'
-  }
-}
+    'ecmaVersion': 'latest',
+  },
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,30 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import VueDevTools from 'vite-plugin-vue-devtools'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import VueDevTools from 'vite-plugin-vue-devtools';
+import eslint from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     VueDevTools(),
+    eslint(),
   ],
+  server: {
+    port: 8080,
+  },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: '@import \'@/assets/scss/globals.scss\';',
+      },
+    },
+  },
+});
