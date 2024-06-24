a8f53c298b867054ad36d29a419b132d44604700
Pedro Henrique <phfs28@gmail.com>
2024-06-24 13:30:31 -0300

    Added logic to load the team list

--- src/views/TeamsView.vue
+++ src/views/TeamsView.vue
@@ -1,3 +1,106 @@
 <template>
-  <div>Equipes</div>
+  <main class="teams">
+    <PageHeader title="Equipes">
+      <AppButton
+        :to="{ name: 'new-team' }"
+        :icon-left="IconPlus"
+      >
+        Nova equipe
+      </AppButton>
+    </PageHeader>
+    <LoadingIndicator v-if="isLoading" />
+    <div
+      v-else
+      class="team-grid"
+    >
+      <RouterLink
+        v-for="team in teams"
+        :key="team.id"
+        class="team-card"
+        to="#"
+      >
+        <div class="team-card__header">
+          <div class="team-card__emblem">
+            <img
+              class="team-card__emblem-image"
+              :src="team.emblem.url"
+              :alt="`${team.name}'s emblem`"
+            />
+          </div>
+          <AppChip :text="team.country">
+            <template #icon-left>
+              <img
+                :src="`/images/country-flag/${team.country}.svg`"
+                alt="Team country flag"
+              />
+            </template>
+          </AppChip>
+        </div>
+        <span>{{ team.name }}</span>
+      </RouterLink>
+    </div>
+  </main>
 </template>
+
+<script lang="ts" setup>
+import type { TeamListItem } from '@/types/Team';
+import { ref } from 'vue';
+import api from '@/api';
+import IconPlus from '@/assets/icons/IconPlus.svg';
+import AppButton from '@/components/AppButton.vue';
+import AppChip from '@/components/AppChip.vue';
+import LoadingIndicator from '@/components/LoadingIndicator.vue';
+import PageHeader from '@/components/PageHeader.vue';
+
+const isLoading = ref(true);
+const teams = ref<TeamListItem[]>([]);
+
+async function getTeams() {
+  try {
+    const { data } = await api.teamService.getTeamList();
+    teams.value = data;
+  } catch (error: any) {
+    console.error(error);
+  } finally {
+    isLoading.value = false;
+  }
+}
+
+getTeams();
+</script>
+
+<style lang="scss" scoped>
+.team-grid {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(17rem, 1fr));
+  gap: 1rem;
+}
+.team-card {
+  padding: 0.75rem;
+  background-color: $color--white;
+  border: 1px solid $color--neutral-300;
+  border-radius: $radius--large;
+  transition: border-color $transition--fastest linear;
+  outline-color: $color--focus-ring;
+  &:hover {
+    border-color: $color--text-300;
+  }
+  &__header {
+    display: flex;
+    justify-content: space-between;
+    align-items: flex-start;
+    margin-bottom: 0.75rem;
+  }
+  &__emblem {
+    width: 2.5rem;
+    height: 2.5rem;
+    padding: 0.25rem;
+    background-color: $color--neutral-200;
+    border-radius: $radius--medium;
+  }
+  &__emblem-image {
+    width: 2rem;
+    height: 2rem;
+  }
+}
+</style>
