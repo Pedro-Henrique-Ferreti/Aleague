4c1cfec7bed2ea4243472c0aee6fa17c9452546f
Pedro Henrique <phfs28@gmail.com>
2024-07-10 09:08:18 -0300

    Added team list card styles

--- src/views/TeamListView.vue
+++ src/views/TeamListView.vue
@@ -33,7 +33,28 @@
           :key="list.id"
           class="list-card"
         >
-          {{ list.name }}
+          <div class="list-card__content">
+            <div
+              class="list-card__flag"
+              :style="`--bg-color: ${CountryColor[list.teams[0].country]}`"
+            >
+              <img
+                class="list-card__flag-image"
+                :src="`/images/country-flag/${list.teams[0].country}.svg`"
+                :alt="list.teams[0].country"
+              />
+            </div>
+            <span class="list-card__title">
+              {{ list.name }}
+            </span>
+            <span>{{ list.teams.length }} equipes</span>
+          </div>
+          <button
+            class="list-card__button"
+            type="button"
+          >
+            Ver lista
+          </button>
         </div>
       </div>
     </TransitionFade>
@@ -44,7 +65,7 @@
 import type { Breadcrumb } from '@/types/Breadcrumb';
 import type { TeamList } from '@/types/Team';
 import { computed, ref } from 'vue';
-import { Country, COUNTRY_OPTIONS } from '@/constants/country';
+import { Country, COUNTRY_OPTIONS, CountryColor } from '@/constants/country';
 import api from '@/api';
 import AppDropdown from '@/components/AppDropdown.vue';
 import PageHeader from '@/components/PageHeader.vue';
@@ -110,5 +131,54 @@ getTeamLists();
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(17rem, 1fr));
   gap: 1rem;
+}
+.list-card {
+  min-height: 16rem;
+  background-color: $color--white;
+  border: 1px solid $color--neutral-300;
+  border-radius: $radius--large;
+  position: relative;
+  &__content {
+    padding: 0.75rem;
+  }
+  &__flag {
+    display: grid;
+    place-items: center;
+    width: 3.5rem;
+    height: 3.5rem;
+    background-color: var(--bg-color);
+    border-radius: 0.5rem;
+  }
+  &__flag-image {
+    width: 2.25rem;
+    height: 2.25rem;
+  }
+  &__title {
+    display: block;
+    margin-top: 0.5rem;
+    margin-bottom: 1.5rem;
+    color: $color--text-strong;
+    font-weight: $font-weight--medium;
+  }
+  &__button {
+    padding: 0.625rem 0;
+    margin-top: auto;
+    border: 1px solid $color--neutral-300;
+    border-bottom-left-radius: inherit;
+    border-bottom-right-radius: inherit;
+    color: $color--blue-800;
+    font-weight: $font-weight--medium;
+    font-size: 0.875rem;
+    text-align: center;
+    transition: border-color $transition--fastest linear;
+    position: absolute;
+    left: -1px;
+    right: -1px;
+    bottom: -1px;
+    outline-color: $color--focus-ring;
+    &:hover {
+      border-color: $color--text-300;
+    }
+  }
 }
 </style>
