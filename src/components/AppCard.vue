c2960fc6c5c2b8677d21368868ccb57d200662ca
Pedro Henrique <phfs28@gmail.com>
2024-07-16 16:46:17 -0300

    Created component AppCard

---
+++ src/components/AppCard.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="card | app-base-card">
+    <div class="card__header">
+      <slot name="header">
+        <h2 class="card__title">
+          {{ title }}
+        </h2>
+      </slot>
+    </div>
+    <slot />
+  </div>
+</template>
+
+<script lang="ts" setup>
+defineProps({
+  title: {
+    type: String,
+    default: '',
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.card {
+  &__header {
+    padding-bottom: 0.75rem;
+    margin-bottom: 1.5rem;
+    border-bottom: 1px solid $color--neutral-300;
+  }
+  &__title {
+    color: $color--text-strong;
+    font-size: 1.25rem;
+    font-weight: $font-weight--medium;
+  }
+}
+</style>
