705f9d6456e4ac3046217a8084ab8c49f3f5f182
Pedro Henrique <phfs28@gmail.com>
2024-07-26 15:43:50 -0300

    Created component AppSelectionCard

---
+++ src/components/AppSelectionCard.vue
@@ -0,0 +1,108 @@
+<template>
+  <label
+    class="selection-card"
+    tabindex="0"
+    :for="id"
+    :data-checked="inputValue === value"
+    @keypress.enter="inputValue = value"
+  >
+    <div class="selection-card__icon-wrapper">
+      <Component
+        class="selection-card__icon"
+        :is="icon"
+      />
+    </div>
+    <input
+      v-model="inputValue"
+      class="selection-card__input"
+      type="radio"
+      tabindex="-1"
+      :id="id"
+      :value="value"
+    />
+    {{ text }}
+  </label>
+</template>
+
+<script lang="ts" setup>
+import { computed } from 'vue';
+
+const emit = defineEmits(['update:modelValue']);
+const props = defineProps({
+  modelValue: {
+    type: String,
+    default: '',
+  },
+  id: {
+    type: String,
+    required: true,
+  },
+  value: {
+    type: String,
+    default: '',
+  },
+  text: {
+    type: String,
+    default: '',
+  },
+  icon: {
+    type: null,
+    required: true,
+  },
+});
+
+const inputValue = computed({
+  get: () => props.modelValue,
+  set(value) {
+    emit('update:modelValue', value);
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.selection-card {
+  --border-color: #{$color--neutral-300};
+  --icon-color: #{$color--text};
+  &[data-checked="true"] {
+    --border-color: #{$color--primary-800};
+    --icon-color: #{$color--primary-800};
+  }
+  width: 10.5rem;
+  height: fit-content;
+  padding: 0.5rem;
+  background-color: $color--neutral-50;
+  border: 1px solid var(--border-color);
+  border-radius: 0.625rem;
+  color: $color--text-strong;
+  cursor: pointer;
+  transition: border-color $transition--fastest linear;
+  &:hover:not([data-checked="true"]) {
+    border-color: $color--text-300;
+  }
+  &__icon-wrapper {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 1.5rem;
+    width: 2.25rem;
+    height: 2.25rem;
+    background-color: $color--white;
+    border: 1px solid $color--neutral-100;
+    border-radius: 0.375rem;
+  }
+  &__icon {
+    width: 100%;
+    height: 100%;
+    max-width: 1.5rem;
+    max-height: 1.5rem;
+    color: var(--icon-color);
+    transition: color $transition--fastest linear;
+  }
+  &__input {
+    width: 0;
+    height: 0;
+    opacity: 0;
+    position: absolute;
+  }
+}
+</style>
