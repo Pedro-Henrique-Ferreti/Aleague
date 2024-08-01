2801e0be28ee464b3074f20121cca52e88c1cb2c
Pedro Henrique <phfs28@gmail.com>
2024-08-01 16:47:50 -0300

    Created tournament card

--- src/views/TournamentsView.vue
+++ src/views/TournamentsView.vue
@@ -53,9 +53,50 @@
         v-else-if="displayedTournaments.length === 0"
         @clear-filters="clearSearchFilters"
       />
-      <pre v-else>
-        {{ displayedTournaments }} {{ form.filterType }}
-      </pre>
+      <div
+        v-else
+        class="tournaments__grid"
+      >
+        <RouterLink
+          v-for="tournament in displayedTournaments"
+          :key="tournament.id"
+          class="tournament-card"
+          :to="'/'"
+        >
+          <div class="tournament-card__header">
+            <div class="tournament-card__icon">
+              <img
+                class="tournament-card__icon-image"
+                alt="Selected icon"
+                :src="`/images/competition-icons/icon-${tournament.icon}.svg`"
+              />
+            </div>
+            <div class="tournament-card__info">
+              <span class="tournament-card__title">
+                {{ tournament.name }}
+              </span>
+              <span class="tournament-card__type">
+                {{ tournament.type }}
+              </span>
+            </div>
+          </div>
+          <AppProgressBar :value="tournament.progress" />
+          <div class="tournament-card__stats">
+            <AppChip
+              :icon-left="IconShield"
+              :text="`${tournament.numberOfTeams} equipes`"
+            />
+            <AppChip
+              :icon-left="IconCalendarAdd"
+              :text="formatDate(tournament.createdAt)"
+            />
+            <AppChip
+              :icon-left="IconPencil"
+              :text="formatDate(tournament.updatedAt)"
+            />
+          </div>
+        </RouterLink>
+      </div>
     </TransitionFade>
   </main>
 </template>
@@ -63,9 +104,16 @@
 <script lang="ts" setup>
 import type { ApiGetAllTournamentsResponse } from '@/types/Tournament';
 import { computed, ref } from 'vue';
+import { format } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
 import api from '@/api';
 import IconPlus from '@/assets/icons/IconPlus.svg';
+import IconCalendarAdd from '@/assets/icons/IconCalendarAdd.svg';
+import IconPencil from '@/assets/icons/IconPencil.svg';
+import IconShield from '@/assets/icons/IconShield.svg';
+import AppChip from '@/components/AppChip.vue';
 import AppButton from '@/components/AppButton.vue';
+import AppProgressBar from '@/components/AppProgressBar.vue';
 import AppRadioInput from '@/components/AppRadioInput.vue';
 import PageHeader from '@/components/PageHeader.vue';
 import SearchBar from '@/components/SearchBar.vue';
@@ -79,6 +127,11 @@ enum FilterType {
   ALL = 0,
   IN_PROGRESS = 1,
   COMPLETED = 2,
+}
+
+// Format date
+function formatDate(date: string) {
+  return format(new Date(date), 'd MMM yyyy', { locale: ptBR });
 }
 
 // Search bar
@@ -137,6 +190,64 @@ getTournaments();
   &__search-bar-radio {
     display: flex;
     gap: 0.75rem;
+  }
+  &__grid {
+    --columns: 1;
+    display: grid;
+    grid-template-columns: repeat(var(--columns), 1fr);
+    gap: 1.5rem;
+    @include for-tablet-landscape-up {
+      --columns: 2;
+    }
+    @include for-desktop-up {
+      --columns: 3;
+    }
+  }
+}
+.tournament-card {
+  padding: 1rem;
+  background-color: $color--white;
+  border: 1px solid $color--neutral-300;
+  border-radius: $radius--large;
+  transition: border-color $transition--fastest linear;
+  outline-color: $color--focus-ring;
+  &:hover {
+    border-color: $color--text-300;
+  }
+  &__header {
+    display: grid;
+    grid-template-columns: auto 1fr;
+    gap: 0.875rem;
+    margin-bottom: 0.75rem;
+  }
+  &__icon {
+    width: 5rem;
+    height: 5rem;
+  }
+  &__icon-image {
+    max-width: 100%;
+    max-height: 100%;
+  }
+  &__info {
+    display: flex;
+    flex-direction: column;
+  }
+  &__title {
+    display: block;
+    padding-bottom: 0.25rem;
+    color: $color--text-strong;
+    font-size: 1.25rem;
+    font-weight: $font-weight--medium;
+  }
+  &__type {
+    margin-top: auto;
+    font-size: 0.875rem;
+  }
+  &__stats {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 0.75rem;
+    margin-top: 1rem;
   }
 }
 </style>
