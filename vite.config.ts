7a18e38bb5c968d651a184747f5eb3c3c7e9d0d1
Pedro Henrique <phsouza@jacksondawson.com>
2024-12-04 08:46:47 -0300

    Disable compiler error

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import VueDevTools from 'vite-plugin-vue-devtools';
+// @ts-expect-error
 import eslint from 'vite-plugin-eslint';
 import svgLoader from 'vite-svg-loader';
 
