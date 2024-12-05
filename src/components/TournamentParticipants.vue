9f18530b1f372ba3eac13df98e2fbfca603c2573
Pedro Henrique <phsouza@jacksondawson.com>
2024-12-05 10:45:11 -0300

    Created component TournamentParticipantsGroup

--- src/components/TournamentParticipants.vue
+++ src/components/TournamentParticipants.vue
@@ -1,16 +1,50 @@
 <template>
-  <div class="container-medium">
+  <div class="stages | container-medium">
     <PageHeader :breadcrumb-items="BREADCRUMB_ITEMS" />
     <TournamentProfileCard :tournament="tournament" />
+    <div class="stages__grid">
+      <AppAccordion
+        v-for="(stage, index) in tournament.stages"
+        :key="stage.id"
+        :title="getStageCardTitle(stage, index)"
+      >
+        <div class="stages__card-header">
+          <AppIconButton
+            v-tooltip="'Duplicar campeonato'"
+            color="secondary"
+            :icon="IconMagicWand"
+          />
+          <AppIconButton
+            v-tooltip="'Embaralhar equipes'"
+            color="secondary"
+            :icon="IconShuffle"
+          />
+        </div>
+        <TournamentParticipantsGroup
+          :selected-teams="[]"
+          :slots="stage.rules.numberOfTeams"
+        />
+      </AppAccordion>
+    </div>
+    <AppButton>
+      Iniciar campeonato
+    </AppButton>
   </div>
 </template>
 
 <script lang="ts" setup>
 import type { PropType } from 'vue';
 import type { Breadcrumb } from '@/types/Breadcrumb';
-import type { Tournament } from '@/types/Tournament';
+import type { Tournament, TournamentStage } from '@/types/Tournament';
+import { TournamentFormat } from '@/constants/tournament';
+import IconMagicWand from '@/assets/icons/MagicWand.svg';
+import IconShuffle from '@/assets/icons/Shuffle.svg';
+import AppAccordion from './AppAccordion.vue';
+import AppButton from './AppButton.vue';
+import AppIconButton from './AppIconButton.vue';
 import PageHeader from './PageHeader.vue';
 import TournamentProfileCard from './TournamentProfileCard.vue';
+import TournamentParticipantsGroup from './TournamentParticipantsGroup.vue';
 
 const props = defineProps({
   tournament: {
@@ -19,6 +53,7 @@ const props = defineProps({
   },
 });
 
+// Breadcrumb items
 const BREADCRUMB_ITEMS: Breadcrumb[] = [
   { title: 'Campeonatos', to: { name: 'tournaments' } },
   {
@@ -27,8 +62,30 @@ const BREADCRUMB_ITEMS: Breadcrumb[] = [
   },
   'Adicionar participantes',
 ];
+
+// Stage card
+function getStageCardTitle(stage: TournamentStage, index: number) {
+  const name = (
+    props.tournament.type === TournamentFormat.ALL_PLAY_ALL
+  ) ? 'Pontos Corridos' : stage.name || `Fase ${index + 1}`;
+
+  return ` ${name} - ${stage.rules.numberOfTeams} equipes`;
+}
 </script>
 
 <style lang="scss" scoped>
-
+.stages {
+  &__grid {
+    display: grid;
+    gap: 1.5rem;
+    margin-top: 2rem;
+    margin-bottom: 2.5rem;
+  }
+  &__card-header {
+    display: flex;
+    justify-content: flex-end;
+    gap: 1rem;
+    margin-bottom: 2rem;
+  }
+}
 </style>
---
+++ src/components/TournamentParticipantsGroup.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="group">
+    <AppChip
+      class="group__chip"
+      color="blue"
+      text="Equipes"
+    />
+    <button
+      v-for="item in slots"
+      class="group__empty-slot-button"
+      type="button"
+      :key="item"
+    >
+      <IconPlus />
+      <span>Adicionar</span>
+    </button>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { computed, type PropType } from 'vue';
+import IconPlus from '@/assets/icons/IconPlus.svg';
+import AppChip from './AppChip.vue';
+
+const emit = defineEmits(['update:selectedTeams']);
+const props = defineProps({
+  selectedTeams: {
+    type: Array as PropType<string[]>,
+    required: true,
+  },
+  slots: {
+    type: Number,
+    required: true,
+  },
+});
+
+const selectedTeamsValue = computed({
+  get: () => props.selectedTeams,
+  set: (value) => emit('update:selectedTeams', value),
+});
+console.log(selectedTeamsValue);
+</script>
+
+<style lang="scss" scoped>
+.group {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(19.25rem, 1fr));
+  gap: 0.5rem;
+  padding: 0.5rem 0.625rem;
+  padding-top: 1.25rem;
+  border: 1px solid $color--neutral-300;
+  border-radius: $radius--large;
+  position: relative;
+  &__chip {
+    position: absolute;
+    top: -0.875rem;
+    left: 50%;
+    transform: translateX(-50%);
+  }
+  &__empty-slot-button {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 0.5rem;
+    height: 2.375rem;
+    background-color: $color--neutral-100;
+    border: 1px dashed $color--neutral-300;
+    border-radius: $radius--medium;
+    font-size: 0.875rem;
+    transition: border-color $transition--fastest;
+    &:hover {
+      border-color: $color--text-400;
+    }
+    svg {
+      width: 0.625rem;
+      height: 0.625rem;
+    }
+  }
+}
+</style>
