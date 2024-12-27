3f818ddbd2feaf5c6a05b3577dca1cc73d47f6d3
Pedro Henrique <phsouza@jacksondawson.com>
2024-12-27 09:36:30 -0300

    Added logic to focus on searchInput when menu is mounted

--- src/components/SelectTeamMenu.vue
+++ src/components/SelectTeamMenu.vue
@@ -3,6 +3,7 @@
     :title="title"
     :confirm-button-text="confirmButtonText"
     :confirm-button-is-disabled="!form.selectedTeamId"
+    @apply-show="focusSearchInput"
     @confirm="selectTeam"
   >
     <slot />
@@ -11,6 +12,7 @@
         v-model="form.searchValue"
         id="select-team-menu-search-input"
         size="small"
+        ref="searchInputRef"
       />
     </template>
     <template #content>
@@ -34,7 +36,9 @@
 
 <script lang="ts" setup>
 import type { SelectTeamMenuOption } from '@/types/SelectTeamMenu';
-import { computed, ref, type PropType } from 'vue';
+import {
+  computed, ref, useTemplateRef, type PropType,
+} from 'vue';
 import AppPopupMenu from './AppPopupMenu.vue';
 import AppSearchInput from './AppSearchInput.vue';
 import SelectTeamMenuItem from './SelectTeamMenuItem.vue';
@@ -65,6 +69,15 @@ const form = ref({
 const displayedTeamOptions = computed(() => props.teamOptions.filter((team) => (
   team.name.toLowerCase().includes(form.value.searchValue.toLowerCase())
 )));
+
+// Focus search input
+const searchInputRef = useTemplateRef('searchInputRef');
+
+function focusSearchInput() {
+  setTimeout(() => {
+    searchInputRef.value?.focus();
+  }, 100);
+}
 
 function selectTeam() {
   emit('select-team', props.teamOptions.find((team) => team.id === form.value.selectedTeamId)!);
