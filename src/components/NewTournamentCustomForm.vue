cc323e0672852e53d8a6ae9ff8b5a41d60202054
Pedro Henrique <phsouza@jacksondawson.com>
2025-02-05 17:12:38 -0300

    Added dashed lines between stage cards

--- src/components/NewTournamentCustomForm.vue
+++ src/components/NewTournamentCustomForm.vue
@@ -1,14 +1,40 @@
 <template>
-  <div class="form">
-    <NewTournamentStageCard
+  <div
+    class="form"
+    :data-empty-list="form.stages.length === 0"
+  >
+    <template
       v-for="(stage, index) in form.stages"
       :key="stage.id"
-      :stage="stage"
-      :order="index + 1"
-    />
-    <AppButton @click="showModal = true">
-      Adicionar fase
-    </AppButton>
+    >
+      <div class="form__button-wrapper">
+        <AppIconButton
+          v-tooltip="'Adicionar fase'"
+          class="form__icon-button"
+          size="small"
+          aria-label="Adicionar fase"
+          :icon="IconPlus"
+        />
+        <IconDashedLine
+          class="form__dashed-line"
+          :viewBox="index === 0 ? '0 0 4 48' : null"
+        />
+      </div>
+      <NewTournamentStageCard
+        :stage="stage"
+        :order="index + 1"
+      />
+    </template>
+    <div class="form__button-wrapper">
+      <IconDashedLine
+        v-if="form.stages.length > 0"
+        class="form__dashed-line"
+        viewBox="0 0 4 48"
+      />
+      <AppButton @click="showModal = true">
+        Adicionar fase
+      </AppButton>
+    </div>
     <NewTournamentStageModal
       :show="showModal"
       @close="showModal = false"
@@ -20,7 +46,10 @@
 <script lang="ts" setup>
 import type { TournamentFormStage } from '@/types/NewTournamentForm';
 import { ref } from 'vue';
+import IconPlus from '@/assets/icons/Plus.svg';
+import IconDashedLine from '@/assets/icons/DashedLine.svg';
 import AppButton from './AppButton.vue';
+import AppIconButton from './AppIconButton.vue';
 import NewTournamentStageModal from './NewTournamentStageModal.vue';
 import NewTournamentStageCard from './NewTournamentStageCard.vue';
 
@@ -35,6 +64,9 @@ const showModal = ref(false);
 <style lang="scss" scoped>
 .form {
   @include scrollbar;
+  &[data-empty-list="true"] {
+    justify-content: center;
+  }
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -46,5 +78,26 @@ const showModal = ref(false);
   background-image: url('/images/dotted-background.svg');
   background-position: center 7%;
   overflow-y: auto;
+  &__button-wrapper {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+    &:first-child .form__icon-button {
+      position: relative;
+    }
+    &:first-child .form__dashed-line,
+    &:last-child .form__dashed-line {
+      height: 3rem;
+    }
+  }
+  &__icon-button {
+    flex-shrink: 0;
+    position: absolute;
+  }
+  &__dashed-line {
+    flex-shrink: 0;
+  }
 }
 </style>
