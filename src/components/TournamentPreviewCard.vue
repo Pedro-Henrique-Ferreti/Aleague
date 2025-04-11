c475885854840eb3130b7561feba4b75d1adbad4
Pedro Henrique <phsouza@jacksondawson.com>
2025-04-11 09:02:25 -0300

    Created component TournamentPreviewCard

---
+++ src/components/TournamentPreviewCard.vue
@@ -0,0 +1,111 @@
+<template>
+  <RouterLink
+    class="tournament-card"
+    :to="{ name: 'show-tournament', params: { id: tournament.id } }"
+  >
+    <div class="tournament-card__header">
+      <div class="tournament-card__icon">
+        <img
+          class="tournament-card__icon-image"
+          alt="Tournament icon"
+          :src="`/images/competition-icons/icon-${tournament.icon}.svg`"
+        />
+      </div>
+      <div class="tournament-card__info">
+        <span class="tournament-card__title">
+          {{ tournament.name }}
+        </span>
+        <span class="tournament-card__type">
+          {{ tournamentTypeText }}
+        </span>
+      </div>
+    </div>
+    <AppProgressBar :value="tournament.progress" />
+    <div class="tournament-card__stats">
+      <AppChip
+        :icon-left="IconCalendarAdd"
+        :text="formatDate(tournament.createdAt, 'd MMM yyyy')"
+      />
+      <AppChip
+        :icon-left="IconPencil"
+        :text="formatDate(tournament.updatedAt, 'd MMM yyyy')"
+      />
+    </div>
+  </RouterLink>
+</template>
+
+<script lang="ts" setup>
+import type { TorunamentPreview } from '@/types/Tournament';
+import { computed } from 'vue';
+import { TournamentFormat } from '@/constants/tournament';
+import { formatDate } from '@/utils';
+import IconCalendarAdd from '@/assets/icons/CalendarAdd.svg';
+import IconPencil from '@/assets/icons/Pencil.svg';
+import AppChip from '@/components/AppChip.vue';
+import AppProgressBar from '@/components/AppProgressBar.vue';
+
+const props = defineProps<{
+  tournament: TorunamentPreview,
+}>();
+
+const tournamentTypeText = computed(() => {
+  switch (props.tournament.type) {
+    case TournamentFormat.ALL_PLAY_ALL:
+      return 'Pontos corridos';
+    case TournamentFormat.PLAYOFFS:
+      return 'Eliminatórias';
+    case TournamentFormat.CUSTOM:
+      return 'Fases livres';
+    default:
+      return '';
+  }
+});
+</script>
+
+<style lang="scss" scoped>
+.tournament-card {
+  padding: 1rem;
+  background-color: $color--white;
+  border: 1px solid $color--neutral-300;
+  border-radius: $radius--large;
+  transition: border-color $transition--fastest linear;
+  outline-color: $color--focus-ring;
+  &:hover {
+    border-color: $color--text-300;
+  }
+  &__header {
+    display: grid;
+    grid-template-columns: auto 1fr;
+    gap: 0.875rem;
+    margin-bottom: 1rem;
+  }
+  &__icon {
+    width: 4rem;
+    height: 4rem;
+  }
+  &__icon-image {
+    max-width: 100%;
+    max-height: 100%;
+  }
+  &__info {
+    display: flex;
+    flex-direction: column;
+  }
+  &__title {
+    display: block;
+    padding-bottom: 0.5rem;
+    color: $color--text-strong;
+    font-size: 1.125rem;
+    font-weight: $font-weight--medium;
+  }
+  &__type {
+    font-size: 0.875rem;
+  }
+  &__stats {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 0.75rem;
+    margin-top: 1rem;
+  }
+}
+</style>
--- src/views/TournamentsView.vue
+++ src/views/TournamentsView.vue
@@ -1,5 +1,5 @@
 <template>
-  <main class="tournaments">
+  <main class="tournaments | container-large">
     <PageHeader
       title="Campeonatos"
       :chip-text="tournaments.length ? String(tournaments.length) : ''"
@@ -34,63 +34,28 @@
         v-else
         class="tournaments__grid"
       >
-        <RouterLink
+        <TournamentPreviewCard
           v-for="tournament in tournaments"
-          class="tournament-card"
           :key="tournament.id"
-          :to="{ name: 'show-tournament', params: { id: tournament.id } }"
-        >
-          <div class="tournament-card__header">
-            <div class="tournament-card__icon">
-              <img
-                class="tournament-card__icon-image"
-                alt="Tournament icon"
-                :src="`/images/competition-icons/icon-${tournament.icon}.svg`"
-              />
-            </div>
-            <div class="tournament-card__info">
-              <span class="tournament-card__title">
-                {{ tournament.name }}
-              </span>
-              <span class="tournament-card__type">
-                {{ getTournamentTypeText(tournament.type) }}
-              </span>
-            </div>
-          </div>
-          <AppProgressBar :value="tournament.progress" />
-          <div class="tournament-card__stats">
-            <AppChip
-              :icon-left="IconCalendarAdd"
-              :text="formatDate(tournament.createdAt, 'd MMM yyyy')"
-            />
-            <AppChip
-              :icon-left="IconPencil"
-              :text="formatDate(tournament.updatedAt, 'd MMM yyyy')"
-            />
-          </div>
-        </RouterLink>
+          :tournament="tournament"
+        />
       </div>
     </TransitionFade>
   </main>
 </template>
 
 <script lang="ts" setup>
-import type { TorunamentPreview, TypeTournamentFormat } from '@/types/Tournament';
+import type { TorunamentPreview } from '@/types/Tournament';
 import { ref } from 'vue';
-import { formatDate } from '@/utils';
-import { TournamentFormat } from '@/constants/tournament';
 import api from '@/api';
 import IconPlus from '@/assets/icons/Plus.svg';
-import IconCalendarAdd from '@/assets/icons/CalendarAdd.svg';
-import IconPencil from '@/assets/icons/Pencil.svg';
-import AppChip from '@/components/AppChip.vue';
 import AppButton from '@/components/AppButton.vue';
-import AppProgressBar from '@/components/AppProgressBar.vue';
 import PageHeader from '@/components/PageHeader.vue';
 import TransitionFade from '@/components/TransitionFade.vue';
 import LoadingIndicator from '@/components/LoadingIndicator.vue';
 import ErrorState from '@/components/ErrorState.vue';
 import EmptyState from '@/components/EmptyState.vue';
+import TournamentPreviewCard from '@/components/TournamentPreviewCard.vue';
 
 // Tournaments
 const tournaments = ref<TorunamentPreview[]>([]);
@@ -114,20 +79,6 @@ async function getTournaments() {
 }
 
 getTournaments();
-
-// Tournament type text
-function getTournamentTypeText(type: TypeTournamentFormat) {
-  switch (type) {
-    case TournamentFormat.ALL_PLAY_ALL:
-      return 'Pontos corridos';
-    case TournamentFormat.PLAYOFFS:
-      return 'Eliminatórias';
-    case TournamentFormat.CUSTOM:
-      return 'Fases livres';
-    default:
-      return '';
-  }
-}
 </script>
 
 <style lang="scss" scoped>
@@ -140,58 +91,14 @@ function getTournamentTypeText(type: TypeTournamentFormat) {
     --columns: 1;
     display: grid;
     grid-template-columns: repeat(var(--columns), 1fr);
-    gap: 1.5rem;
-    @include for-large-tablet-landscape-up {
+    gap: 0.75rem;
+    @include for-large-tablet-portrait-up {
       --columns: 2;
     }
-    @media (min-width: 1600px) {
+    @media (min-width: 1400px) {
       --columns: 3;
     }
   }
 }
-.tournament-card {
-  padding: 1rem;
-  background-color: $color--white;
-  border: 1px solid $color--neutral-300;
-  border-radius: $radius--large;
-  transition: border-color $transition--fastest linear;
-  outline-color: $color--focus-ring;
-  &:hover {
-    border-color: $color--text-300;
-  }
-  &__header {
-    display: grid;
-    grid-template-columns: auto 1fr;
-    gap: 0.875rem;
-    margin-bottom: 1rem;
-  }
-  &__icon {
-    width: 4rem;
-    height: 4rem;
-  }
-  &__icon-image {
-    max-width: 100%;
-    max-height: 100%;
-  }
-  &__info {
-    display: flex;
-    flex-direction: column;
-  }
-  &__title {
-    display: block;
-    padding-bottom: 0.5rem;
-    color: $color--text-strong;
-    font-size: 1.125rem;
-    font-weight: $font-weight--medium;
-  }
-  &__type {
-    font-size: 0.875rem;
-  }
-  &__stats {
-    display: flex;
-    flex-wrap: wrap;
-    gap: 0.75rem;
-    margin-top: 1rem;
-  }
-}
+
 </style>
