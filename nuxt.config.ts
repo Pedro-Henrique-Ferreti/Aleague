8b930263dea14e4bec97a56ed8323a8da33dde25
Pedro Henrique <phsouza@jacksondawson.com>
2025-09-18 10:12:32 -0300

    feat: add Tailwindcss and DaisyUI

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,21 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/icon']
+  modules: ['@nuxt/eslint', '@nuxt/icon'],
+  app: {
+    head: {
+      title: 'Aleague',
+    },
+  },
+  devServer: {
+    port: 8080,
+  },
+  css: ['@/assets/css/main.css'],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
 })
