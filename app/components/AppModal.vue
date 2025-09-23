9346c82c4d4c2e0dd0b0606d5bda9d7be5c05823
Pedro Henrique <phsouza@jacksondawson.com>
2025-09-23 13:42:04 -0300

    feat(modal): create component AppModal

---
+++ app/components/AppModal.vue
@@ -0,0 +1,67 @@
+<template>
+  <slot
+    name="trigger"
+    :open="openModal"
+    :close="closeModal"
+    :toggle="toggleModal"  
+  />
+  <dialog
+    ref="dialogRef"
+    class="modal"
+    :id="id"
+  >
+    <div class="modal-box">
+      <form method="dialog">
+        <button class="btn btn-sm btn-circle btn-ghost absolute right-1 top-1">✕</button>
+      </form>
+      <h3
+        v-if="title"
+        v-text="title"
+        class="text-lg font-semibold mb-1"
+      />
+      <slot />
+      <div class="modal-action">
+        <AppButton
+          class="btn-ghost"
+          label="Cancelar"
+          @click="closeModal"
+        />
+        <slot name="actions" />
+      </div>
+    </div>
+  </dialog>
+</template>
+
+<script lang="ts" setup>
+const id = useId();
+
+const emit = defineEmits<{
+  (e: 'open'): void;
+  (e: 'close'): void;
+}>();
+defineProps<{
+  title?: string;
+}>();
+
+const dialogRef = ref<HTMLDialogElement | null>(null);
+
+function openModal() {
+  dialogRef.value?.showModal();
+  emit('open');
+}
+
+function closeModal() {
+  dialogRef.value?.close();
+  emit('close');
+}
+
+function toggleModal() {
+  (dialogRef.value?.open) ? dialogRef.value.close() : dialogRef.value?.showModal();
+}
+
+defineExpose({
+  open: openModal,
+  close: closeModal,
+  toggle: toggleModal
+});
+</script>
