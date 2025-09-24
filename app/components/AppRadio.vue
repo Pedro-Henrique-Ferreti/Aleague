173adcafb1017a23a8b1d88a49659bc41d7d80b3
Pedro Henrique <phsouza@jacksondawson.com>
2025-09-24 13:38:47 -0300

    feat(radio): create component AppRadio

---
+++ app/components/AppRadio.vue
@@ -0,0 +1,27 @@
+<template>
+  <label class="inline-flex gap-0.5" :for="id">
+    <input
+      v-model="model"
+      type="radio"
+      class="radio"
+      :id="id"
+      :name="name"
+      :value="value"
+    />
+    <span class="text-sm">
+      <slot>{{ label }}</slot>
+    </span>
+  </label>
+</template>
+
+<script lang="ts" setup generic="T extends string | number">
+const id = useId();
+
+defineProps<{
+  name: string;
+  value: T;
+  label?: string;
+}>();
+
+const model = defineModel<T>();
+</script>
