d9d282e4e20adc6052a9700ae7b2ee0d69a5c628
Pedro Henrique <phsouza@jacksondawson.com>
2025-09-26 14:16:22 -0300

    feat(groups): display standings

--- app/components/TournamentGroupCard.vue
+++ app/components/TournamentGroupCard.vue
@@ -1,8 +1,8 @@
 <template>
-  <section class="card card-border">
+  <section class="card card-border overflow-hidden">
     <div class="card-body gap-0 p-1.25">
       <header class="flex justify-between">
-        <div class="text-xl font-medium">{{ title }}</div>
+        <div class="text-lg font-medium">{{ title }}</div>
         <AppButton
           v-if="showFilters"
           class="btn-square btn-ghost w-2 h-2"
@@ -14,12 +14,85 @@
         v-if="showFilters"
         v-show="filtersVisible"
       />
-      <div class="divider" />
+      <div class="divider mt-0.5 -mb-[7px]" />
+      <div class="table-container">
+        <div
+          ref="tableWrapperRef"
+          class="overflow-x-auto"
+        >
+          <table class="table static">
+            <tbody>
+              <tr
+                class="text-xs text-gray-500 bg-gray-1 [&_th]:p-0.75 [&_th]:text-center"
+                key="header"
+              >
+                <th
+                  class="resize-observer"
+                  aria-hidden="true"
+                />
+                <th class="position bg-inherit">
+                  Posição
+                </th>
+                <th class="points">
+                  <span class="desktop:hidden">Pts</span>
+                  <span class="max-desktop:hidden">Pontos</span>
+                </th>
+                <th>
+                  <span class="desktop:hidden">J</span>
+                  <span class="max-desktop:hidden">Jogos</span>
+                </th>
+                <th>
+                  <span class="desktop:hidden">V</span>
+                  <span class="max-desktop:hidden">Vitórias</span>
+                </th>
+                <th>
+                  <span class="desktop:hidden">E</span>
+                  <span class="max-desktop:hidden">Empates</span>
+                </th>
+                <th>
+                  <span class="desktop:hidden">D</span>
+                  <span class="max-desktop:hidden">Derrotas</span>
+                </th>
+                <th>GP</th>
+                <th>GC</th>
+                <th>SG</th>
+                <th>%</th>
+                <th>Recentes</th>
+                <th />
+              </tr>
+              <tr
+                v-for="entry in standings"
+                class="bg-white hover:bg-gray-1 transition-colors duration-300 text-center [&_td]:px-0.75"
+                :key="entry.id"
+              >
+                <td
+                  v-resize-observer="onResizeObserver"
+                  class="resize-observer p-0!"
+                />
+                <td class="position bg-inherit">Palmeiras</td>
+                <td class="font-semibold">20</td>
+                <td>{{ entry.played }}</td>
+                <td>{{ entry.won }}</td>
+                <td>{{ entry.drawn }}</td>
+                <td>{{ entry.lost }}</td>
+                <td>{{ entry.goalsFor }}</td>
+                <td>{{ entry.goalsAgainst }}</td>
+                <td>{{ entry.goalsFor - entry.goalsAgainst }}</td>
+                <td>{{ entry.played ? Math.round(entry.points / (entry.played * 3) * 100) : 0 }}%</td>
+                <td />
+                <td />
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   </section>
 </template>
 
 <script lang="ts" setup>
+import type { ResizeObserverCallback } from '@vueuse/core';
+import { vResizeObserver } from '@vueuse/components';
 import { IconAdjustmentsHorizontal } from '@tabler/icons-vue';
 
 defineProps<{
@@ -29,4 +102,41 @@ defineProps<{
 }>();
 
 const filtersVisible = ref(false);
+
+// Position size
+const positionSize = ref('0px');
+
+const onResizeObserver: ResizeObserverCallback = (entries) => {
+  positionSize.value = `${entries[0]?.contentRect.width || 0}px`;
+}
+
+const tableWrapperRef = useTemplateRef<HTMLDivElement>('tableWrapperRef');
+const tableScroll = useScroll(tableWrapperRef);
+
+const rightBorderSize = computed(() => {
+  const clientWidth = tableWrapperRef.value?.clientWidth || 0;
+  const scrollWidth = tableWrapperRef.value?.scrollWidth || 0;
+  const scrollSize = scrollWidth - clientWidth;
+
+  return `${scrollSize - tableScroll.x.value}px`;
+});
 </script>
+
+<style scoped>
+@reference '@/assets/css/main.css';
+
+.table-container {
+  @apply relative;
+}
+.table-container::after {
+  width: v-bind(rightBorderSize);
+  @apply content-[''] max-w-[2.25rem] h-[calc(100%-1rem)] bg-gradient-to-l from-white to-transparent absolute right-0 top-0 pointer-events-none;
+}
+.resize-observer {
+  @apply min-w-16 w-[40%];
+}
+.position {
+  width: v-bind(positionSize);
+  @apply text-left absolute left-0;
+}
+</style>
