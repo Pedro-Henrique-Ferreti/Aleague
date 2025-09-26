587aac2bbdb82a2a9a5e4a9abf3273fd931b9e62
Pedro Henrique <phsouza@jacksondawson.com>
2025-09-26 15:18:12 -0300

    feat(stage): create component EditStageTeamsModal

---
+++ app/components/EditStageTeamsModal.vue
@@ -0,0 +1,12 @@
+<template>
+  <AppButton
+    class="btn-primary btn-soft"
+    label="Equipes"
+    :class="$attrs.class"
+    :icon-left="IconUsersGroup"
+  />
+</template>
+
+<script lang="ts" setup>
+import { IconUsersGroup } from '@tabler/icons-vue';
+</script>
