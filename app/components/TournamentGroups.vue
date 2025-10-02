c93365615fa4c66531182e455552cb3431408934
Pedro Henrique <phsouza@jacksondawson.com>
2025-10-02 14:34:34 -0300

    fix(standings): prevent Standings cards from stretching vertically

--- app/components/TournamentGroups.vue
+++ app/components/TournamentGroups.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="grid gap-2 tablet-lg:grid-cols-[2fr_1fr]">
-    <div class="grid gap-1.5">
+    <div class="grid gap-1.5 h-fit">
       <StandingsCard
         v-for="(group, index) in stage.groups"
         :key="group.order"
