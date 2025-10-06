a64ec33414d5f63776d45fb990f18bf8afde4209
Pedro Henrique <phsouza@jacksondawson.com>
2025-10-06 13:40:37 -0300

    feat(input): add model modifiers lazy and trim

--- app/components/BaseInput.vue
+++ app/components/BaseInput.vue
@@ -1,11 +1,13 @@
 <template>
   <input
-    v-model.trim="model"
+    :value="model"
     :type="type"
     :name="name"
     :id="id || defaultId"
     :readonly="!!readonly"
     :disabled="disabled"
+    @input="onInput"
+    @change="onChange"
   >
 </template>
 
@@ -18,6 +20,8 @@ interface BaseInputProps {
   readonly?: boolean;
   disabled?: boolean;
   type?: InputTypeHTMLAttribute;
+  modelValue?: BaseInputModel;
+  modelModifiers?: { lazy?: boolean; trim?: boolean };
 }
 </script>
 
@@ -26,9 +30,29 @@ import type { InputTypeHTMLAttribute } from 'vue';
 
 const defaultId = useId();
 
-withDefaults(defineProps<BaseInputProps>(), {
+const emit = defineEmits<{
+  'update:modelValue': [value: BaseInputModel];
+}>();
+const props = withDefaults(defineProps<BaseInputProps>(), {
   type: 'text',
 });
 
-const model = defineModel<BaseInputModel>();
+const model = computed({
+  get: () => props.modelValue,
+  set: (value: BaseInputModel) => {
+    emit('update:modelValue', (
+      props.modelModifiers?.trim && value && typeof value === 'string' ? value?.trim() : value
+    ));
+  },
+});
+
+function onInput(event: Event) {
+  if (props.modelModifiers?.lazy) return;
+  model.value = (event.target as HTMLInputElement).value;
+}
+
+function onChange(event: Event) {
+  if (!props.modelModifiers?.lazy) return;
+  model.value = (event.target as HTMLInputElement).value;
+}
 </script>
