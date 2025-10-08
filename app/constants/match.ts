6a21e41c6df7b0c92242c197920ded628ffb959a
Pedro Henrique <phsouza@jacksondawson.com>
2025-10-08 11:12:09 -0300

    feat(match): add match kickoff options

---
+++ app/constants/match.ts
@@ -0,0 +1,30 @@
+import { type Day, setDay } from 'date-fns';
+
+function kickoffTime(weekDay: Weekday, hours: number, minutes: number, weekStartsOn: Day = 0): string {
+  return setDay(new Date(2025, 10, 8, hours, minutes), weekDay, { weekStartsOn }).toISOString();
+}
+
+export const KICKOFF_TIMES: Record<MatchweekKickoff, string[]> = {
+  [MatchweekKickoff.WEEKDAY]: [
+    kickoffTime(Weekday.TUESDAY, 21, 0),
+    kickoffTime(Weekday.WEDNESDAY, 17, 0),
+    kickoffTime(Weekday.WEDNESDAY, 19, 0),
+    kickoffTime(Weekday.WEDNESDAY, 21, 0),
+    kickoffTime(Weekday.THURSDAY, 19, 0),
+    kickoffTime(Weekday.THURSDAY, 20, 0),
+  ],
+  [MatchweekKickoff.WEEKEND]: [
+    kickoffTime(Weekday.SATURDAY, 16, 0, 6),
+    kickoffTime(Weekday.SATURDAY, 18, 0, 6),
+    kickoffTime(Weekday.SATURDAY, 20, 30, 6),
+    kickoffTime(Weekday.SUNDAY, 16, 0, 6),
+    kickoffTime(Weekday.SUNDAY, 18, 30, 6),
+    kickoffTime(Weekday.SUNDAY, 20, 30, 6),
+    kickoffTime(Weekday.MONDAY, 20, 0, 6),
+  ],
+};
+
+export const MATCHWEEK_KICKOFF_OPTIONS: SelectOptionList<MatchweekKickoff> = [
+  { label: 'Durante a semana', value: MatchweekKickoff.WEEKDAY },
+  { label: 'Fim de semana', value: MatchweekKickoff.WEEKEND },
+];
--- app/types/Match.ts
+++ app/types/Match.ts
@@ -1,3 +1,18 @@
+export enum MatchweekKickoff {
+  WEEKDAY,
+  WEEKEND,
+}
+
+export enum Weekday {
+  SUNDAY,
+  MONDAY,
+  TUESDAY,
+  WEDNESDAY,
+  THURSDAY,
+  FRIDAY,
+  SATURDAY,
+}
+
 export interface MatchTeam {
   id: Team['id'];
   score: number | null;
