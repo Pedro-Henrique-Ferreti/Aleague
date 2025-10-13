311ed978ab35eb2ae7c60a80be817f4062f0f603
Pedro Henrique <phsouza@jacksondawson.com>
2025-10-13 16:49:31 -0300

    feat(playoffs): delete round second match

--- app/components/PlayoffRoundCard.vue
+++ app/components/PlayoffRoundCard.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="flex h-full items-center">
-    <div class="card card-border p-0.75 order-1 w-full relative group">
+    <div class="card card-border dropdown dropdown-hover dropdown-right p-0.75 order-1 w-full group">
       <MatchCard
         v-model:home-score="slot.matches[0].homeTeam.score"
         v-model:away-score="slot.matches[0].awayTeam.score"
+        role="button"
+        tabindex="0"
         layout="vertical"
         :match="slot.matches[0]"
         :fixture-two-home-score="slot.matches[1]?.homeTeam.score"
@@ -11,20 +13,29 @@
         @update:fixture-two-home-score="slot.matches[1] ? slot.matches[1].homeTeam.score = $event as Match['homeTeam']['score'] : null"
         @update:fixture-two-away-score="slot.matches[1] ? slot.matches[1].awayTeam.score = $event as Match['awayTeam']['score'] : null"
       />
-      <AppButton
-        v-if="slot.matches.length <= 1"
-        v-tooltip="'Adicionar jogo de volta'"
-        class="btn-square btn-primary btn-xs rounded-md absolute! top-1/2 -translate-y-1/2! right-0 translate-x-1/2 hidden group-[:hover]:block"
-        :icon-left="IconPlus"
-        @click="addMatchToSlot"
-      />
+      <div class="dropdown-content menu bg-base-100 w-9 rounded-box z-1 shadow-sm">
+        <AppButton
+          v-if="slot.matches.length <= 1"
+          class="btn-primary btn-soft btn-sm"
+          label="Jogo de volta"
+          :icon-left="IconPlus"
+          @click="addMatchToSlot"
+        />
+        <AppButton
+          v-else
+          class="btn-error btn-soft btn-sm"
+          label="Jogo de volta"
+          :icon-left="IconTrash"
+          @click="slot.matches.pop()"
+        />
+      </div>
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
 import { v4 as uuidv4 } from 'uuid';
-import { IconPlus } from '@tabler/icons-vue';
+import { IconPlus, IconTrash } from '@tabler/icons-vue';
 
 const slot = defineModel<PlayoffRound['slots'][number]>('slot', { required: true });
 
