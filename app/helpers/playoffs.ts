7947e01697799091351999732ba4ab76b0a40605
Pedro Henrique <phsouza@jacksondawson.com>
2025-10-13 09:00:17 -0300

    refactor: remove use of for loop in getPlayoffRoundNames

--- app/helpers/playoffs.ts
+++ app/helpers/playoffs.ts
@@ -1,13 +1,9 @@
 export function getPlayoffRoundNames(rounds: number, teams: number): string[] {
-  let n = teams;
-
-  for (let i = 0; i < rounds; i += 1) {
-    n /= 2;
-  }
+  const isFullPath = (teams / 2 ** rounds) === 1;
 
   return Array.from({ length: rounds }, (_, i) => {
     const name = PLAYOFF_ROUND_NAMES[i - (rounds - PLAYOFF_ROUND_NAMES.length)]
     
-    return (n !== 1 || !name) ? `Fase ${i + 1}` : name;
+    return (!isFullPath || !name) ? `Fase ${i + 1}` : name;
   });
 }
