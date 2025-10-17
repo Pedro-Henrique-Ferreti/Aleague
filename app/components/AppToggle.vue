826509cc4942c927451c046c04e766bd46b63b3f
Pedro Henrique <phsouza@jacksondawson.com>
2025-10-17 19:21:56 -0300

    feat(toggle): create component AppToggle

---
+++ app/components/AppToggle.vue
@@ -0,0 +1,19 @@
+<template>
+  <label class="flex gap-0.5 items-center">
+    <input
+      v-model="model"
+      type="checkbox"
+      class="toggle"
+      :id="id"
+    />
+    <span>{{ label }}</span>
+  </label>
+</template>
+
+<script lang="ts" setup>
+const id = useId();
+
+defineProps<{ label: string }>();
+
+const model = defineModel<boolean>();
+</script>
