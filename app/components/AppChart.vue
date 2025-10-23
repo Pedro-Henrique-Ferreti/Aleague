19f28908bc0de13589966fb9ee032d48237b15d6
Pedro Henrique <phsouza@jacksondawson.com>
2025-10-23 10:24:28 -0300

    feat(chart): install libray Chart.js

---
+++ app/components/AppChart.vue
@@ -0,0 +1,42 @@
+<template>
+  <canvas :id="id" />
+</template>
+
+<script lang="ts" setup>
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  LineController,
+  LineElement,
+  PointElement,
+  Filler,
+  Tooltip,
+  type ChartConfiguration,
+} from 'chart.js';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  LineController,
+  LineElement,
+  PointElement,
+  Filler,
+  Tooltip,
+);
+
+const id = useId();
+
+const props = defineProps<{
+  config: ChartConfiguration,
+}>();
+
+onMounted(() => {
+  const ctx = (document.getElementById(id) as HTMLCanvasElement).getContext('2d');
+
+  if (ctx) {
+    const chart = new ChartJS(ctx, props.config);
+    chart.render();
+  }
+});
+</script>
