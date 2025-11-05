07c80bf23d4c9e2fcdb68ea688ca86d9ea409404
Pedro Henrique <phsouza@jacksondawson.com>
2025-11-05 10:59:43 -0300

    feat(standings): create component StandingsFormList

---
+++ app/components/StandingsFormList.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="font-semibold mb-0.5">Sequência</div>
+  <swiper-container
+    space-between="16"
+    free-mode="true"
+    slides-per-view="auto"
+  >
+    <swiper-slide
+      v-for="form in entry?.form"
+      :key="form.match.id"
+      class="w-max last:mr-0!"
+      data-allow-mismatch="class"
+    >
+      <StandingsFormItem
+        class="min-h-7"
+        :form="form"
+        :team-id="entry?.team || null"
+      />
+    </swiper-slide>
+  </swiper-container>
+</template>
+
+<script lang="ts" setup>
+defineProps<{
+  entry: TableEntry | null;
+}>();
+</script>
--- app/components/StandingsFormModal.vue
+++ app/components/StandingsFormModal.vue
@@ -5,25 +5,10 @@
     :title="`Performance: ${getTeamById(entry?.team)?.name}`"
     :show-actions="false"
   >
-    <div class="font-semibold mb-0.5">Sequência</div>
-    <swiper-container
-      space-between="16"
-      free-mode="true"
-      slides-per-view="auto"
-    >
-      <swiper-slide
-        v-for="form in entry?.form"
-        :key="form.match.id"
-        class="w-max last:mr-0!"
-        data-allow-mismatch="class"
-      >
-        <StandingsFormItem
-          class="min-h-7"
-          :form="form"
-          :team-id="entry?.team || null"
-        />
-      </swiper-slide>
-    </swiper-container>
+    <StandingsFormList
+      :key="entry?.team || ''"
+      :entry="entry"
+    />
     <div class="font-semibold mt-2 mb-0.5">Posição por rodada</div>
     <StandingsFormChart
       v-if="chartData"
