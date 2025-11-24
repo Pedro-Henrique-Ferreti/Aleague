8a1d022bd35c6c52b42a6e73d78c3ba16d5e60f6
Pedro Henrique <phsouza@jacksondawson.com>
2025-11-24 11:52:44 -0300

    feat(sorting): add tab sorting

--- app/components/TournamentList.vue
+++ app/components/TournamentList.vue
@@ -1,27 +1,49 @@
 <template>
   <div
     role="tablist"
-    class="tabs tabs-lift"
+    class="tabs tabs-lift border-b border-b-base-300"
+    ref="tablist"
   >
-    <template
-      v-for="(tournament, index) in tournamentStore.tournaments"
-      :key="tournament.id"
-    >
-      <AppTab
-        :label="tournament.name"
-        :is-active="tournamentStore.activeTournamentId === tournament.id"
-        @click="tournamentStore.activeTournamentId = tournament.id"
-      />
-      <div
-        v-if="tournamentStore.activeTournamentId === tournament.id"
-        class="tab-content pt-3 border-b-0 border-l-0 border-r-0 rounded-none border-base-300"
-      >
-        <TournamentPage v-model="tournamentStore.tournaments[index]!" />
-      </div>
-    </template>
+  <AppTab
+    v-for="tournament in tournamentStore.tournaments"
+    class="-mb-px"
+    :key="tournament.id"
+    :label="tournament.name"
+    :is-active="tournamentStore.activeTournamentId === tournament.id"
+    @mousedown="tournamentStore.activeTournamentId = tournament.id"
+  />
+  </div>
+  <div class="tab-content block pt-3 border-b-0 border-l-0 border-r-0">
+    <TournamentPage
+      v-if="activeTournament"
+      v-model="activeTournament"
+    />
   </div>
 </template>
 
 <script setup lang="ts">
+import Sortable from 'sortablejs';
+
 const tournamentStore = useTournamentStore();
+
+const activeTournament = computed({
+  get: () => tournamentStore.activeTournament,
+  set: (tournament) => {
+    const index = tournamentStore.tournaments.findIndex(t => t.id === tournament?.id);
+    
+    if (index > -1 && tournament) {
+      tournamentStore.tournaments[index] = tournament;
+    }
+  },
+});
+
+const tablistRef = useTemplateRef('tablist');
+
+onMounted(() => {
+  new Sortable(tablistRef.value, {
+    ghostClass: 'opacity-0',
+    easing: 'cubic-bezier(0.45, 0, 0.55, 1)',
+    animation: 250,
+  });
+});
 </script>
