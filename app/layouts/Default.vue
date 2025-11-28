065be805ba1e076d6271e15cfc6f0b2c2764316e
Pedro Henrique <phsouza@jacksondawson.com>
2025-11-28 13:37:39 -0300

    refactor: create component TheNavbar

--- app/layouts/Default.vue
+++ app/layouts/Default.vue
@@ -1,22 +1,7 @@
 <template>
   <div class="container-lg pt-1.5 pb-3">
     <header class="flex gap-2 justify-between mb-2">
-      <nav class="flex gap-1">
-        <AppButton
-          class="btn-ghost bg-white hover:border-gray-300 hover:bg-white"
-          label="Campeonatos"
-          :class="[$route.name === 'home' ? 'text-black! border-gray-300' : 'border-transparent']"
-          :to="{ name: 'home' }"
-          :icon-left="IconSitemap"
-        />
-        <AppButton
-          class="btn-ghost bg-white hover:border-gray-300 hover:bg-white"
-          label="Estatísticas"
-          :class="[$route.name === 'analytics' ? 'text-black! border-gray-300' : 'border-transparent']"
-          :to="{ name: 'analytics' }"
-          :icon-left="IconDatabase"
-        />
-      </nav>
+      <TheNavbar />
       <div class="flex gap-1 justify-end">
         <slot name="header" />
       </div>
@@ -26,7 +11,3 @@
     </main>
   </div>
 </template>
-
-<script setup lang="ts">
-import { IconDatabase, IconSitemap } from '@tabler/icons-vue';
-</script>
