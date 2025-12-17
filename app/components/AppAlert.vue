092b714e22afd546f7d6adb65df0c33df88557fd
Pedro Henrique <phsouza@jacksondawson.com>
2025-12-17 11:35:45 -0300

    feat(alert): create component AppAlert

---
+++ app/components/AppAlert.vue
@@ -0,0 +1,34 @@
+<template>
+  <div
+    class="alert"
+    role="alert"
+    :class="{
+      'alert-info': type === 'info',
+      'alert-success': type === 'success',
+      'alert-warning': type === 'warning',
+      'alert-error': type === 'error',
+      'alert-soft': soft,
+    }"
+  >
+    <slot name="icon">
+      <component
+        class="shrink-0"
+        :is="icon"
+      />
+    </slot>
+    <slot>
+      <span>{{ message }}</span>
+    </slot>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { Icon } from '@tabler/icons-vue';
+
+defineProps<{
+  icon?: Icon;
+  message?: string;
+  soft?: boolean;
+  type?: 'success' | 'error' | 'warning' | 'info';
+}>()
+</script>
