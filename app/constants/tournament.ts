7efdb70bb577bdb0591558b81ffe13414bbfa2ea
Pedro Henrique <phsouza@jacksondawson.com>
2026-01-19 19:01:35 -0300

    feat(tournament): add brazilian tournament logos

--- app/constants/tournament.ts
+++ app/constants/tournament.ts
@@ -8,6 +8,13 @@ export const ICON_OPTIONS: Readonly<SelectOptionList<Tournament['icon']>> = [
       value: `/images/icons/tournament/icon-${index + 1}.svg`,
     }),
   ),
+  { label: 'Campeonato Paulista 2026', value: '/images/icons/tournament/Paulistao_2026.png' },
+  { label: 'Campeonato Paulista A2 2025', value: '/images/icons/tournament/Paulistao_A2_2025.png' },
+  { label: 'Campeonato Brasileiro Série A', value: '/images/icons/tournament/Campeonato_Brasileiro_Serie_A.png' },
+  { label: 'Campeonato Brasileiro Série B', value: '/images/icons/tournament/Campeonato_Brasileiro_Serie_B.png' },
+  { label: 'Campeonato Brasileiro Série C', value: '/images/icons/tournament/Campeonato_Brasileiro_Serie_C.png' },
+  { label: 'Copa Libertadores', value: '/images/icons/tournament/Conmebol_Libertadores.png' },
+  { label: 'Copa Sul-americana', value: '/images/icons/tournament/Conmebol_Sudamericana.png' },
 ];
 
 export const TOURNAMENT_GROUP_FORMAT_OPTIONS: Readonly<SelectOptionList<TournamentGroupFormat>> = [
