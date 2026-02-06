3fda1c1dd9de5029969838bcad9cd80b92fbbdac
Pedro Henrique <phsouza@jacksondawson.com>
2026-02-06 15:42:31 -0300

    feat(ui): create component CardRadio

---
+++ app/components/CardRadio.vue
@@ -0,0 +1,37 @@
+<template>
+  <label
+    class="card card-border rounded-lg"
+    :for="id"
+    :class="{
+      'cursor-pointer': !disabled,
+      'border-base-content': model === value,
+    }"
+  >
+    <div class="card-body p-0.75 flex-row justify-between">
+      <div>
+        <slot :is-checked="model === value" />
+      </div>
+      <input
+        v-model="model"
+        type="radio"
+        class="radio size-1.25"
+        :id="id"
+        :name="name"
+        :value="value"
+        :disabled="disabled"
+      >
+    </div>
+  </label>
+</template>
+
+<script lang="ts" setup generic="T extends string | number | null">
+const id = useId();
+
+defineProps<{
+  name: string;
+  value: T;
+  disabled?: boolean;
+}>();
+
+const model = defineModel<T>();
+</script>
