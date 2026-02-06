e78427ca979808d5521f36e5650e6982bbdd923f
Pedro Henrique <phsouza@jacksondawson.com>
2026-02-06 12:52:28 -0300

    feat(breadcrumbs): create components BreadcrumbList and BreadcrumbItem

---
+++ app/components/BreadcrumbItem.vue
@@ -0,0 +1,21 @@
+<template>
+  <li>
+    <div class="cursor-[unset] hover:no-underline">
+      <component
+        v-if="icon"
+        :is="icon"
+        class="h-1 w-1"
+      />
+      <slot>{{ label }}</slot>
+    </div>
+  </li>
+</template>
+
+<script lang="ts" setup>
+import type { Icon } from '@tabler/icons-vue';
+
+defineProps<{
+  label?: string;
+  icon?: Icon;
+}>();
+</script>
