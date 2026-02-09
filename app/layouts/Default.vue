1af1da4eccaf57cbd106b4ea6bc9b3cbe34b2a9e
Pedro Henrique <phsouza@jacksondawson.com>
2026-02-09 16:37:11 -0300

    refactor(layout): collapse header border

--- app/layouts/Default.vue
+++ app/layouts/Default.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="grid grid-cols-[auto_1fr]">
     <TheSidebar />
-    <div class="container-base pt-1.5 pb-3 px-2 desktop-xl:pr-3">
-      <header class="pb-0.75 mb-2 border-b border-base-200 empty:hidden">
+    <div class="container-base pt-0.75 pb-3 px-0">
+      <header class="pb-0.75 mb-2 border-b border-base-200 px-2 desktop-xl:pr-3 empty:hidden">
         <slot name="header" />
       </header>
-      <main class="@container/main">
+      <main class="@container/main px-2 desktop-xl:pr-3">
         <slot />
       </main>
     </div>
