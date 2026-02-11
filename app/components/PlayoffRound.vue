f8431dcda81141a61d9a240b41ecd075f83dc05f
Pedro Henrique <phsouza@jacksondawson.com>
2026-02-11 11:05:15 -0300

    feat(playoff): add edit icon to round name input

--- app/components/PlayoffRound.vue
+++ app/components/PlayoffRound.vue
@@ -1,11 +1,14 @@
 <template>
-  <div class="grid gap-1.5 grid-rows-[auto_1fr] not-first:[&_.bracket]:mb-0! first:[&_.bracket]:before:hidden! first:[&_.bracket]:after:hidden!">
-    <input
-      v-model.lazy="name"
-      type="text"
-      class="w-full text-lg text-center font-medium"
-      :id="inputId"
-    >
+  <div class="round">
+    <div class="relative group">
+      <input
+        v-model.lazy="name"
+        type="text"
+        class="w-full text-lg text-center font-medium focus:[&+svg]:hidden"
+        :id="inputId"
+      >
+      <IconPencil class="round-edit-icon" />
+    </div>
     <div class="flex flex-col gap-0.75">
       <PlayoffRoundCard
         v-for="(slot, index) in slots"
@@ -20,6 +23,8 @@
 </template>
 
 <script lang="ts" setup>
+import { IconPencil } from '@tabler/icons-vue';
+
 const inputId = useId();
 
 defineEmits<{
@@ -32,6 +37,14 @@ const slots = defineModel<PlayoffRound['slots']>('slots', { required: true });
 
 <style scoped>
 @reference '@/assets/css/main.css';
+
+.round {
+  @apply grid gap-1.5 grid-rows-[auto_1fr] not-first:[&_.bracket]:mb-0! first:[&_.bracket]:before:hidden! first:[&_.bracket]:after:hidden!;
+}
+
+.round-edit-icon {
+  @apply size-1.25 text-base-content/60 fill-white absolute right-0.5 top-1/2 -translate-y-1/2 pointer-events-none not-group-hover:hidden;
+}
 
 .bracket::before, .bracket::after {
   @apply block shrink-0 border border-gray-300 content-[''];
