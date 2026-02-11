b57ff163300745037dc5e491ef6d697f64345303
Pedro Henrique <phsouza@jacksondawson.com>
2026-02-11 10:20:35 -0300

    feat(ui): add primary type to AppMenuItem

--- app/components/AppMenuItem.vue
+++ app/components/AppMenuItem.vue
@@ -3,7 +3,10 @@
     <button
       type="button"
       class="whitespace-nowrap"
-      :class="{ 'text-error hover:bg-error/8 active:bg-error! active:text-error-content!': type === 'error' }"
+      :class="{
+        'text-error hover:bg-error/8 active:bg-error! active:text-error-content!': type === 'error',
+        'text-primary hover:bg-primary/8 active:bg-primary! active:text-primary-content!': type === 'primary',
+      }"
       @click="$emit('click')"
     >
       <component
@@ -18,10 +21,12 @@
 <script lang="ts" setup>
 import type { Icon } from '@tabler/icons-vue';
 
+defineEmits<{
+  click: [];
+}>();
 defineProps<{
   label: string;
   icon: Icon;
-  type?: 'error';
+  type?: 'primary' | 'error';
 }>();
 </script>
-
