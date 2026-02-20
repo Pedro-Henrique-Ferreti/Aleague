9f3b22986b7e59a7935ea2381545b17b89a7a3b2
Pedro Henrique <phsouza@jacksondawson.com>
2026-02-20 19:43:29 -0300

    refactor(tournament): delete types file TournamentStore

--- app/types/Form.ts
+++ app/types/Form.ts
@@ -1,3 +1,15 @@
+export type TournamentForm = Pick<Tournament, 'name' | 'icon' | 'tags' | 'showCountry'>;
+
+export type StageForm = (
+  Pick<BaseStage, 'name' | 'type'>
+  & {
+    teams: number;
+    teamsPerGroup: number;
+    groups: number;
+    playoffRounds: number;
+  }
+);
+
 export interface StageTeamsForm {
   groups: {
     order: number;
