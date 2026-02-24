9e843699470a054decef0f258fbb7cbd9331e337
Pedro Henrique <phsouza@jacksondawson.com>
2026-02-24 17:56:43 -0300

    fix(matchweek): MatchCard stretching to fill available height

--- app/components/MatchweekFormModalPreviewCard.vue
+++ app/components/MatchweekFormModalPreviewCard.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="card card-border relative">
-    <div class="card-body p-1 pt-1.5 grid gap-0.5">
+    <div class="card-body p-1 pt-1.5 flex flex-col gap-0.75">
       <div
         class="badge badge-soft absolute top-0 left-1/2 -translate-1/2"
         :class="[roundRobinNumber % 2 ? 'badge-secondary' : 'badge-accent']"
