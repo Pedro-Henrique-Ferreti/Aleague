d4a624e28ed4cd35969f7e6a8bfe88db65b8d351
Pedro Henrique <phsouza@jacksondawson.com>
2026-02-25 16:42:05 -0300

    feat(ui): create component LoadingIndicator

---
+++ app/components/LoadingIndicator.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="flex flex-col items-center justify-center text-center">
+    <div class="w-full max-w-32">
+      <p class="mb-0.5">{{ message }}</p>
+      <AppProgress />
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+interface Props {
+  message?: string;
+}
+
+withDefaults(defineProps<Props>(), {
+  message: 'Carregando. Por favor, aguarde...',
+});
+</script>
