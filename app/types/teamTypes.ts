abeaae80fd7f52f121164187835d09c5f9b5250d
Pedro Henrique <phsouza@jacksondawson.com>
2026-05-07 16:55:17 -0300

    refactor: update team id from number to string

--- app/types/teamTypes.ts
+++ app/types/teamTypes.ts
@@ -5,7 +5,7 @@ export enum TeamType {
 }
 
 export interface TeamDetails {
-  id: number;
+  id: string;
   name: string;
   country: string;
   badge: string;
--- test/nuxt/match-schedule.test.ts
+++ test/nuxt/match-schedule.test.ts
@@ -3,8 +3,8 @@ import { addRoundRobins, balanceScheduleWeeks, createMatchSchedule, generateInit
 import { getExpectedMatchweeksPerRoundRobin, getMaxPossibleMatchweeksPerRoundRobin } from '~/helpers/matchweek';
 
 const unbalanceableSchedule = {
-  teams: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20],
-  avoidGroups: [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16], [17, 18, 19, 20]],
+  teams: createArray(20, String),
+  avoidGroups: [['1', '2', '3', '4'], ['5', '6', '7', '8'], ['9', '10', '11', '12'], ['13', '14', '15', '16'], ['17', '18', '19', '20']],
 };
 
 const EXECUTION_TIME = MAX_EXECUTION_TIME + 1000;
@@ -12,42 +12,42 @@ const EXECUTION_TIME = MAX_EXECUTION_TIME + 1000;
 describe('match-schedule', () => {
   describe('generateInitialSchedule', () => {
     it('should throw error when teams array has odd number of elements', () => {
-      const teams = [1, 2, 3, 4, 5];
+      const teams = ['1', '2', '3', '4', '5'];
       expect(() => generateInitialSchedule(teams)).toThrow();
     });
 
     it('should throw error when teams array has duplicate elements', () => {
-      const teams = [1, 2, 3, 4, 6, 6];
+      const teams = ['1', '2', '3', '4', '6', '6'];
       expect(() => generateInitialSchedule(teams)).toThrow();
     });
 
     it('should throw error when an avoid group has odd number of elements', () => {
-      const teams = [1, 2, 3, 4];
-      const avoidGroups = [[1, 2, 3]];
+      const teams = ['1', '2', '3', '4'];
+      const avoidGroups = [['1', '2', '3']];
       expect(() => generateInitialSchedule(teams, avoidGroups)).toThrow();
     });
 
     it('should throw error when avoid groups contain different number of elements', () => {
-      const teams = [1, 2, 3, 4];
-      const avoidGroups = [[1, 2], [3, 4, 5, 6]];
+      const teams = ['1', '2', '3', '4'];
+      const avoidGroups = [['1', '2'], ['3', '4', '5', '6']];
       expect(() => generateInitialSchedule(teams, avoidGroups)).toThrow();
     });
 
     it('should throw error when teams and avoidGroups do not have exact same elements', () => {
-      const teams = [1, 2, 3, 4];
-      const avoidGroups = [[1, 2], [5, 6]];
+      const teams = ['1', '2', '3', '4'];
+      const avoidGroups = [['1', '2'], ['5', '6']];
       expect(() => generateInitialSchedule(teams, avoidGroups)).toThrow();
     });
 
     it('should ensure no team plays against itself', () => {
-      const teams = [1, 2, 3, 4];
+      const teams = ['1', '2', '3', '4'];
       const schedule = generateInitialSchedule(teams);
 
       expect(schedule.every(week => week.every(match => match.homeTeam.id !== match.awayTeam.id))).toBeTruthy();
     });
 
     it('should ensure weeks do not have duplicate teams', () => {
-      const teams = [1, 2, 3, 4];
+      const teams = ['1', '2', '3', '4'];
       const schedule = generateInitialSchedule(teams);
 
       schedule.forEach((week) => {
@@ -58,9 +58,9 @@ describe('match-schedule', () => {
     });
 
     it('should ensure a schedule with no avoidGroups has the correct number of weeks', () => {
-      const teams1 = createArray(4);
-      const teams2 = createArray(8);
-      const teams3 = createArray(20);
+      const teams1 = createArray(4, String);
+      const teams2 = createArray(8, String);
+      const teams3 = createArray(20, String);
 
       expect(generateInitialSchedule(teams1).length).toBe(getMaxPossibleMatchweeksPerRoundRobin(teams1.length));
       expect(generateInitialSchedule(teams2).length).toBe(getMaxPossibleMatchweeksPerRoundRobin(teams2.length));
@@ -68,7 +68,7 @@ describe('match-schedule', () => {
     });
 
     it('should ensure a schedule with no avoidGroups has each team playing in every week', () => {
-      const teams = [1, 2, 3, 4];
+      const teams = ['1', '2', '3', '4'];
       const schedule = generateInitialSchedule(teams);
 
       schedule.forEach((week) => {
@@ -81,8 +81,8 @@ describe('match-schedule', () => {
 
   describe('balanceScheduleWeeks', () => {
     it('should ensure all weeks have the same number of matches', async () => {
-      const teams = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-      const avoidGroups = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]];
+      const teams = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'];
+      const avoidGroups = [['1', '2', '3', '4'], ['5', '6', '7', '8'], ['9', '10', '11', '12']];
       const schedule = generateInitialSchedule(teams, avoidGroups);
       const { schedule: balancedSchedule } = await balanceScheduleWeeks(schedule, teams.length, avoidGroups[0]!.length);
       const expectedWeeks = getExpectedMatchweeksPerRoundRobin(teams.length, avoidGroups[0]!.length);
@@ -130,7 +130,7 @@ describe('match-schedule', () => {
 
   describe('addRoundRobins', () => {
     it('should add the correct number of round robins', () => {
-      const schedule = generateInitialSchedule([1, 2, 3, 4]);
+      const schedule = generateInitialSchedule(createArray(4, String));
 
       expect(addRoundRobins(schedule).length).toBe(3);
       expect(addRoundRobins(schedule, 2).length).toBe(6);
@@ -140,7 +140,7 @@ describe('match-schedule', () => {
 
   describe('createMatchSchedule', () => {
     it('should cap the number of weeks to the number provided', async () => {
-      const teams = [1, 2, 3, 4];
+      const teams = createArray(4, String);
       const { schedule } = await createMatchSchedule({ teams, weeksToCreate: 2 });
 
       expect(schedule.length).toBe(2);
