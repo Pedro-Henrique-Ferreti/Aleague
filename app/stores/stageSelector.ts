658c0e0ef99ae912da7c00ee5281cbaa2d76baec
Pedro Henrique <phsouza@jacksondawson.com>
2026-06-06 11:07:14 -0300

    fix(stage): select first playoff round or last group stage

--- app/stores/stageSelector.ts
+++ app/stores/stageSelector.ts
@@ -1,6 +1,8 @@
 interface SelectionOption {
   id: GroupStage['id'] | PlayoffRound['id'];
   name: GroupStage['name'] | PlayoffRound['name'];
+  type: StageType;
+  isFirstStageRound?: boolean;
 }
 
 export const useStageSelectorStore = defineStore('stage-selector', () => {
@@ -9,19 +11,27 @@ export const useStageSelectorStore = defineStore('stage-selector', () => {
   const selectedStageOrPlayoffRoundId = ref<SelectionOption['id']>();
 
   const selectionOptions = computed(() => {
-    const stages = tournamentStore.activeTournament?.stages || [];
+    const stages = tournamentStore.activeTournament?.stages ?? [];
 
     return stages.flatMap((stage): SelectionOption[] => {
       if (stage.type === StageType.GROUP) {
-        return [{ id: stage.id, name: stage.name }];
+        return [{ id: stage.id, name: stage.name, type: StageType.GROUP }];
       }
 
-      return stage.rounds.map(round => ({ id: round.id, name: round.name }));
+      return stage.rounds.map((round, index): SelectionOption => ({
+        id: round.id,
+        name: round.name,
+        type: StageType.PLAYOFF,
+        isFirstStageRound: index === 0,
+      }));
     });
   });
 
   watchEffect(() => {
-    selectedStageOrPlayoffRoundId.value = selectionOptions.value[selectionOptions.value.length - 1]?.id;
+    const lastOption = selectionOptions.value[selectionOptions.value.length - 1];
+    const lastFirstRoundOption = selectionOptions.value.findLast(option => option.isFirstStageRound);
+
+    selectedStageOrPlayoffRoundId.value = (lastOption?.type === StageType.GROUP ? lastOption : lastFirstRoundOption)?.id;
   });
 
   return {
