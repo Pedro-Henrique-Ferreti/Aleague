ea8cd56f819a31f9db27a541ad74db4dc530bbfd
Pedro Henrique <phsouza@jacksondawson.com>
2026-08-07 20:50:05 -0300

    refactor(file): create component FileToolbarTournament

--- app/components/FileToolbar.vue
+++ app/components/FileToolbar.vue
@@ -16,27 +16,7 @@
       v-if="tournamentStore.activeTournament"
       class="flex justify-end gap-0.5 ml-auto border-l border-base-200 pl-0.5"
     >
-      <FileToolbarButton
-        type="delete"
-        label="Excluir"
-        :icon-left="IconTrash"
-        @click="deleteTournamentDialogIsOpen = true"
-      />
-      <FileToolbarButton
-        label="Mover"
-        :icon-left="IconFolderUp"
-        @click="moveTournamentModalIsOpen = true"
-      />
-      <FileToolbarButton
-        label="Duplicar"
-        :icon-left="IconLayersSubtract"
-        @click="tournamentStore.duplicateActiveTournament"
-      />
-      <FileToolbarButton
-        label="Editar"
-        :icon-left="IconEdit"
-        @click="tournamentModalIsOpen = true"
-      />
+      <FileToolbarTournament />
       <AppButton
         class="btn-accent btn-soft ml-0.5"
         label="Salvar"
@@ -45,28 +25,14 @@
       />
     </div>
   </div>
-  <template v-if="tournamentStore.activeTournament">
-    <TournamentFormModal
-      v-model:is-open="tournamentModalIsOpen"
-      :key="tournamentId"
-      :tournament="tournamentStore.activeTournament"
-      :submit-fn="tournamentStore.updateActiveTournament"
-    />
-    <MoveTournamentModal v-model:is-open="moveTournamentModalIsOpen" />
-    <DeleteTournamentModal v-model:is-open="deleteTournamentDialogIsOpen" />
-  </template>
 </template>
 
 <script lang="ts" setup>
-import { IconDownload, IconEdit, IconFileDescription, IconFolderOpen, IconFolderUp, IconLayersSubtract, IconTrash } from '@tabler/icons-vue';
+import { IconDownload, IconFileDescription, IconFolderOpen } from '@tabler/icons-vue';
 
 const tournamentStore = useTournamentStore();
 const collectionStore = useCollectionStore();
 const { downloadSourceFile } = useExportSourceFile();
 
 const tournamentId = computed(() => tournamentStore.activeTournamentId!);
-
-const tournamentModalIsOpen = ref(false);
-const moveTournamentModalIsOpen = ref(false);
-const deleteTournamentDialogIsOpen = ref(false);
 </script>
---
+++ app/components/FileToolbarTournament.vue
@@ -0,0 +1,43 @@
+<template>
+  <FileToolbarButton
+    type="delete"
+    label="Excluir"
+    :icon-left="IconTrash"
+    @click="deleteTournamentDialogIsOpen = true"
+  />
+  <FileToolbarButton
+    label="Mover"
+    :icon-left="IconFolderUp"
+    @click="moveTournamentModalIsOpen = true"
+  />
+  <FileToolbarButton
+    label="Duplicar"
+    :icon-left="IconLayersSubtract"
+    @click="tournamentStore.duplicateActiveTournament"
+  />
+  <FileToolbarButton
+    label="Editar"
+    :icon-left="IconEdit"
+    @click="tournamentModalIsOpen = true"
+  />
+  <TournamentFormModal
+    v-model:is-open="tournamentModalIsOpen"
+    :key="tournamentId"
+    :tournament="tournamentStore.activeTournament"
+    :submit-fn="tournamentStore.updateActiveTournament"
+  />
+  <MoveTournamentModal v-model:is-open="moveTournamentModalIsOpen" />
+  <DeleteTournamentModal v-model:is-open="deleteTournamentDialogIsOpen" />
+</template>
+
+<script lang="ts" setup>
+import { IconEdit, IconFolderUp, IconLayersSubtract, IconTrash } from '@tabler/icons-vue';
+
+const tournamentStore = useTournamentStore();
+
+const tournamentId = computed(() => tournamentStore.activeTournamentId!);
+
+const tournamentModalIsOpen = ref(false);
+const moveTournamentModalIsOpen = ref(false);
+const deleteTournamentDialogIsOpen = ref(false);
+</script>
