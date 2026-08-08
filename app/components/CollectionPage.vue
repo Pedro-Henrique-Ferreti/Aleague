0a5d2e198203090ae9ba45da6d347dca57109327
Pedro Henrique <phsouza@jacksondawson.com>
2026-08-07 21:36:30 -0300

    feat(collection): display collection tournaments

--- app/components/CollectionPage.vue
+++ app/components/CollectionPage.vue
@@ -1,3 +1,22 @@
 <template>
   <CollectionProfileCard />
+  <section class="mt-2">
+    <h2 class="font-medium text-2xl mb-1">Campeonatos</h2>
+    <div class="grid gap-1 grid-cols-[repeat(auto-fill,minmax(17rem,1fr))]">
+      <CollectionTournamentCard
+        v-for="tournament in tournaments"
+        :key="tournament.id"
+        :tournament="tournament"
+      />
+    </div>
+  </section>
 </template>
+
+<script setup lang="ts">
+const collectionStore = useCollectionStore();
+const tournamentStore = useTournamentStore();
+
+const tournaments = computed(() => {
+  return tournamentStore.tournaments.filter(t => t.collectionId === collectionStore.activeCollectionId);
+});
+</script>
---
+++ app/components/CollectionTournamentCard.vue
@@ -0,0 +1,23 @@
+<template>
+  <BaseButton
+    class="card card-border cursor-pointer transition-colors hover:bg-base-content/10"
+    @click="tournamentStore.activeTournamentId = tournament.id"
+  >
+    <div class="card-body p-0.75 flex-row items-center text-left gap-0.75">
+      <img
+        class="w-auto h-3"
+        alt="Logo do campeonato"
+        :src="tournament.icon"
+      >
+      {{ tournament.name }}
+    </div>
+  </BaseButton>
+</template>
+
+<script lang="ts" setup>
+defineProps<{
+  tournament: Tournament;
+}>();
+
+const tournamentStore = useTournamentStore();
+</script>
