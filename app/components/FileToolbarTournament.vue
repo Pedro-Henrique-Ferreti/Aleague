6ca1ed96c5cda31f1f6c408acab52acca7d4bdb8
Pedro Henrique <phsouza@jacksondawson.com>
2026-08-21 15:48:44 -0300

    feat(tournament): remove the Move Tournament functionality

--- app/components/FileToolbarTournament.vue
+++ app/components/FileToolbarTournament.vue
@@ -6,11 +6,6 @@
     @click="deleteTournamentDialogIsOpen = true"
   />
   <FileToolbarButton
-    label="Mover"
-    :icon-left="IconFolderUp"
-    @click="moveTournamentModalIsOpen = true"
-  />
-  <FileToolbarButton
     label="Duplicar"
     :icon-left="IconLayersSubtract"
     @click="tournamentStore.duplicateActiveTournament"
@@ -27,12 +22,11 @@
     :tournament="tournamentStore.activeTournament"
     :submit-fn="tournamentStore.updateActiveTournament"
   />
-  <MoveTournamentModal v-model:is-open="moveTournamentModalIsOpen" />
   <DeleteTournamentModal v-model:is-open="deleteTournamentDialogIsOpen" />
 </template>
 
 <script lang="ts" setup>
-import { IconEdit, IconFolderUp, IconLayersSubtract, IconTrash } from '@tabler/icons-vue';
+import { IconEdit, IconLayersSubtract, IconTrash } from '@tabler/icons-vue';
 
 const tournamentStore = useTournamentStore();
 const { downloadTournamentFile } = useExportSourceFile();
@@ -40,6 +34,5 @@ const { downloadTournamentFile } = useExportSourceFile();
 const tournamentId = computed(() => tournamentStore.activeTournamentId!);
 
 const tournamentModalIsOpen = ref(false);
-const moveTournamentModalIsOpen = ref(false);
 const deleteTournamentDialogIsOpen = ref(false);
 </script>
