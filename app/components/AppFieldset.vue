8722fbe3c01b5e89194a5f3e54d3a5847ba2d680
Pedro Henrique <phsouza@jacksondawson.com>
2026-09-14 15:41:40 -0300

    fix(ui): remove top margin from AppFieldset label

--- app/components/AppFieldset.vue
+++ app/components/AppFieldset.vue
@@ -1,6 +1,6 @@
 <template>
   <fieldset class="fieldset py-0">
-    <legend class="fieldset-legend">{{ label }}</legend>
+    <legend class="fieldset-legend pt-0">{{ label }}</legend>
     <slot />
   </fieldset>
 </template>
