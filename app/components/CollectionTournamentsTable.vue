4be31ac0bc4e05707e0f49b248cd00803d64d76f
Pedro Henrique <phsouza@jacksondawson.com>
2026-09-24 10:16:53 -0300

    feat(collection): add border to CollectionTournamentsTable

--- app/components/CollectionTournamentsTable.vue
+++ app/components/CollectionTournamentsTable.vue
@@ -1,50 +1,52 @@
 <template>
-  <div class="overflow-x-auto">
-    <table class="table table-empty-cell-content">
-      <thead>
-        <tr>
-          <th>Nome</th>
-          <th>Categoria</th>
-          <th>Vencedor</th>
-          <th>Última modificação</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr
-          v-for="tournament in tournaments"
-          :key="tournament.id"
-          class="hover:bg-base-content/10 transition-colors cursor-pointer"
-          tabindex="0"
-          @click="tournamentStore.activeTournamentId = tournament.id"
-        >
-          <td>
-            <div class="flex items-center gap-1">
-              <img
-                class="w-auto max-w-2 h-2"
-                alt="Logo do campeonato"
-                :src="tournament.icon"
-              >
-              {{ tournament.name }}
-            </div>
-          </td>
-          <td>
-            <span
-              v-for="tag, index in tournament.tags"
-              :key="index"
-              class="badge badge-ghost badge-sm"
-            >{{ tag }}</span>
-          </td>
-          <td>
-            <TeamDetails
-              v-if="winners[tournament.id]"
-              :team="winners[tournament.id]!"
-              :show-country="tournament.showCountry"
-            />
-          </td>
-          <td>{{ formatDate(tournament?.updatedAt, 'd MMM, yyyy') }}</td>
-        </tr>
-      </tbody>
-    </table>
+  <div class="card card-border overflow-x-auto">
+    <div class="card-body p-1">
+      <table class="table table-empty-cell-content">
+        <thead>
+          <tr>
+            <th>Nome</th>
+            <th>Categoria</th>
+            <th>Vencedor</th>
+            <th>Última modificação</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr
+            v-for="tournament in tournaments"
+            :key="tournament.id"
+            class="hover:bg-base-content/10 transition-colors cursor-pointer"
+            tabindex="0"
+            @click="tournamentStore.activeTournamentId = tournament.id"
+          >
+            <td>
+              <div class="flex items-center gap-1">
+                <img
+                  class="w-auto max-w-2 h-2"
+                  alt="Logo do campeonato"
+                  :src="tournament.icon"
+                >
+                {{ tournament.name }}
+              </div>
+            </td>
+            <td>
+              <span
+                v-for="tag, index in tournament.tags"
+                :key="index"
+                class="badge badge-ghost badge-sm"
+              >{{ tag }}</span>
+            </td>
+            <td>
+              <TeamDetails
+                v-if="winners[tournament.id]"
+                :team="winners[tournament.id]!"
+                :show-country="tournament.showCountry"
+              />
+            </td>
+            <td>{{ formatDate(tournament?.updatedAt, 'd MMM, yyyy') }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
